25061c2f31687f7c89930ce8aa03ec7f83ced5e5
akrherz <akrherz@iastate.edu>
2014-07-15 14:59:53 -0500

    upstream fix for mods

--- scripts/cligen/modify_idep1.py
+++ scripts/cligen/modify_idep1.py
@@ -4,19 +4,22 @@ Adjust IDEPv1 climate files to match our reality
 import glob
 import os
 
-os.chdir('/mnt/idep/data/clifiles/')
-for fn in glob.glob("*.dat"):
-    data = open(fn).readlines()
-    if data[4].find(" 1997 ") == 0:
-        continue
-    print 'Modify: %s' % (fn,)
-    o = open(fn, 'w')
-    newdata = False
-    for i, line in enumerate(data):
-        if i == 4:
-            line = line.replace("1997", "2007").replace(" 17 ", " 7 ")
-        if not newdata and line.find("1\t1\t2007") == 0:
-            newdata = True
-        if i < 15 or newdata:
-            o.write(line)
-    o.close()
+os.chdir('/i/cli/')
+for d in glob.glob("*"):
+    os.chdir(d)
+    for fn in glob.glob("*.cli"):
+        data = open(fn).readlines()
+        if data[4].find(" 1997 ") == 0:
+            continue
+        print 'Modify: %s' % (fn,)
+        o = open(fn, 'w')
+        newdata = False
+        for i, line in enumerate(data):
+            if i == 4:
+                line = line.replace("1997", "2007").replace(" 17 ", " 7 ")
+            if not newdata and line.find("1\t1\t2007") == 0:
+                newdata = True
+            if i < 15 or newdata:
+                o.write(line)
+        o.close()
+    os.chdir("..")
