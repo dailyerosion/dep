2d255a28cfc9169364bd5e9017f075b19ae8f5b8
akrherz <akrherz@iastate.edu>
2014-08-18 13:12:34 -0500

    add scenario support

---
+++ database/upgrade/4.sql
@@ -0,0 +1,14 @@
+--- Add scenario support
+create table scenarios (id int UNIQUE, label varchar);
+
+insert into scenarios(id, label) values (0, 'Production');
+
+alter table flowpath_points add scenario int references scenarios(id);
+alter table flowpaths add scenario int references scenarios(id);
+alter table results add scenario int references scenarios(id);
+alter table results_by_huc12 add scenario int references scenarios(id);
+
+update flowpath_points set scenario = 0;
+update flowpaths set scenario = 0;
+update results set scenario = 0;
+update results_by_huc12 set scenario = 0;
--- scripts/util/make_dirs.py
+++ scripts/util/make_dirs.py
@@ -1,6 +1,9 @@
 """ Setup the necessary directory structure, so that other codes need not """
 import psycopg2
 import os
+import sys
+
+SCENARIO = sys.argv[1]
 
 PGCONN = psycopg2.connect(database='idep', host='iemdb')
 cursor = PGCONN.cursor()
@@ -10,12 +13,13 @@ PREFIXES = 'env  man  prj  run  slp  sol  wb'.split()
 def do(huc12):
     """ Directory creator! """
     for prefix in PREFIXES:
-        d = "/i/%s/%s/%s" % (prefix, huc12[:8], huc12[8:])
+        d = "/i/%s/%s/%s/%s" % (SCENARIO, prefix, huc12[:8], huc12[8:])
         if not os.path.isdir(d):
             os.makedirs(d)
 
 if __name__ == '__main__':
     # Go Main Go
-    cursor.execute("""SELECT distinct huc_12 from flowpaths""")
+    cursor.execute("""SELECT distinct huc_12 from flowpaths
+    WHERE scenario = %s""", (SCENARIO,))
     for row in cursor:
         do( row[0] )
