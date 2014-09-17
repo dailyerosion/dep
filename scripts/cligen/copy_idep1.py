9bebdf929ee53a439bf8de5fa5a6583241a8a852
akrherz <akrherz@iastate.edu>
2014-09-17 13:48:53 -0500

    account for the new reality that is storing clifile in database

--- scripts/cligen/copy_idep1.py
+++ scripts/cligen/copy_idep1.py
@@ -17,42 +17,45 @@ icursor = idep.cursor()
 wepp = psycopg2.connect(database='wepp', host='iemdb')
 wcursor = wepp.cursor()
 
-
-IDEPHOME = "/i"
+def get_latlon(clifile):
+    """ Convert a clifile name into lon and latitude """
+    fn = clifile.split("/")[-1]
+    lat = float(fn[7:13])
+    lon = 0 - float(fn[0:7])
+    return lon, lat
 
 icursor.execute("""
-  SELECT distinct
-  round(ST_X(ST_PointN(ST_TRANSFORM(geom,4326), 1))::numeric,2),
-  round(ST_Y(ST_PointN(ST_TRANSFORM(geom,4326), 1))::numeric,2)
-  from flowpaths where substr(huc_12,1,8) in ('07020009', '07100007')
-""")
+  SELECT distinct climate_file from flowpaths WHERE scenario = %s
+  and climate_file is not null
+""", (SCENARIO,) )
 print 'Found %s distinct IDEPv2 Precip Cells' % (icursor.rowcount,)
 for row in icursor:
+    clifile = row[0]
+    if os.path.isfile(clifile):
+        continue
+    lon, lat = get_latlon( clifile )
     wcursor.execute("""
     select hrap_i from hrap_polygons where 
     ST_contains(the_geom, ST_Transform(ST_Geometryfromtext(
-    'SRID=4326;POINT(%s %s)'),26915)) and used""" % (row[0], row[1]))
+    'SRID=4326;POINT(%s %s)'),26915)) and used""" % (lon, lat))
     row2 = wcursor.fetchone()
     if row2 is None:
         # Find the closest hrap_polygon
         wcursor.execute("""select hrap_i, 
         ST_Distance(ST_Geometryfromtext('SRID=4326;POINT(%s %s)'), 
         ST_Transform(the_geom,4326)) from hrap_polygons WHERE used
-        ORDER by st_distance ASC LIMIT 1""" % (row[0], row[1]))
+        ORDER by st_distance ASC LIMIT 1""" % (lon, lat))
         row2 = wcursor.fetchone()
         print '---> No HRAP Polygon for Lon: %.3f Lat: %.3f, assign: %s' % (
-                                        row[0], row[1], row2[0])
+                                        lon, lat, row2[0])
         
     hrap = row2[0]
     
     oldfn = "/mnt/idep/data/clifiles/%s.dat"  % (hrap,)
-    newdir = "/i/%s/cli/%03.0fx%03.0f" % (SCENARIO, 0 - row[0], row[1])
-    newfn = "%s/%06.2fx%06.2f.cli" % (newdir, 0 - row[0], row[1])
+    newdir = os.path.basename(clifile)
     if not os.path.isdir(newdir):
         os.makedirs(newdir)
-    if os.path.isfile(newfn):
-        #continue
-        os.unlink(newfn)
-    #print oldfn, newfn
-    shutil.copyfile(oldfn, newfn)
+
+    print oldfn, clifile
+    shutil.copyfile(oldfn, clifile)
     #subprocess.call("ln -s %s %s" % (oldfn, newfn), shell=True)
