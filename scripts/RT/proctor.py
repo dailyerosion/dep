057fdb4c378f7245fdf99e7c7104a2dc8e6ffc3e
akrherz <akrherz@iastate.edu>
2014-10-01 13:20:39 -0500

    use clifile from database and fix pylint whining

--- scripts/RT/proctor.py
+++ scripts/RT/proctor.py
@@ -2,33 +2,28 @@
  I do the realtime run!
 '''
 import psycopg2
-import os
 import sys
-import time
 import subprocess
-import threading
 import datetime
 from multiprocessing import Pool
 
 SCENARIO = sys.argv[1]
 IDEPHOME = "/i/%s" % (SCENARIO,) # don't need trailing /
 
-class wepprun:
-    ''' Represents a single run of WEPP 
-    
+class wepprun(object):
+    """ Represents a single run of WEPP
+
     Filenames have a 51 character restriction
-    '''
-    
-    def __init__(self, huc12, fpid, lon, lat):
-        ''' We initialize with a huc12 identifier and a flowpath id '''
+    """
+
+    def __init__(self, huc12, fpid, clifile):
+        """ We initialize with a huc12 identifier and a flowpath id """
         self.huc12 = huc12
         self.huc8 = huc12[:8]
         self.subdir = "%s/%s" % (huc12[:8], huc12[8:])
         self.fpid = fpid
-        self.lon = lon
-        self.lat = lat
-        pass
-    
+        self.clifile = clifile
+
     def get_wb_fn(self):
         ''' Return the water balance filename for this run '''
         return '%s/wb/%s/%s_%s.wb' % (IDEPHOME, self.subdir,
@@ -38,38 +33,36 @@ class wepprun:
         ''' Return the event filename for this run '''
         return '%s/env/%s/%s_%s.env' % (IDEPHOME, self.subdir,
                                            self.huc12, self.fpid)
+
     def get_error_fn(self):
         ''' Return the event filename for this run '''
         return '%s/error/%s/%s_%s.env' % (IDEPHOME, self.subdir,
                                            self.huc12, self.fpid)
     def get_man_fn(self):
         ''' Return the management filename for this run '''
-        return '%s/man/%s/%s_%s.man' % (IDEPHOME, self.subdir, 
+        return '%s/man/%s/%s_%s.man' % (IDEPHOME, self.subdir,
                                                   self.huc12, self.fpid)
-    
+
     def get_slope_fn(self):
         ''' Return the slope filename for this run '''
-        return '%s/slp/%s/%s_%s.slp' % (IDEPHOME, self.subdir, 
+        return '%s/slp/%s/%s_%s.slp' % (IDEPHOME, self.subdir,
                                              self.huc12, self.fpid)
 
     def get_soil_fn(self):
         ''' Return the soil filename for this run '''
-        return '%s/sol/%s/%s_%s.sol' % (IDEPHOME, self.subdir, 
+        return '%s/sol/%s/%s_%s.sol' % (IDEPHOME, self.subdir,
                                             self.huc12, self.fpid)
-    
+
     def get_clifile_fn(self):
         ''' Return the climate filename for this run '''
-        return '%s/cli/%03.0fx%03.0f/%06.2fx%06.2f.cli' % (IDEPHOME, 
-                                                           0 - self.lon, 
-                                                  self.lat, 0 - self.lon,
-                                                  self.lat)
+        return '%s/%s' % (IDEPHOME, self.clifile)
 
     def get_runfile_fn(self):
         ''' Return the run filename for this run '''
         return '%s/run/%s/%s_%s.run' % (IDEPHOME, self.subdir,
                                                self.huc12, self.fpid)
 
-    
+
     def make_runfile(self):
         ''' Create a runfile for our runs '''
         #print 'Creating runfile for HUC12: %s HS: %s' % (self.huc12, self.fpid)
@@ -126,34 +119,29 @@ def realtime_run():
     idep = psycopg2.connect(database='idep', host='iemdb', user='nobody')
     icursor = idep.cursor()
 
-    icursor.execute("""SELECT huc_12, fid, fpath, 
-    round(ST_X(ST_PointN(ST_TRANSFORM(geom,4326), 1))::numeric,2),
-    round(ST_Y(ST_PointN(ST_TRANSFORM(geom,4326), 1))::numeric,2) 
+    icursor.execute("""SELECT huc_12, fid, fpath, climate_file
     from flowpaths where scenario = %s""" % (SCENARIO,))
     for row in icursor:
-        QUEUE.append( row )
-    
-    
+        QUEUE.append(row)
+
+
 def run(row):
     """ Run ! """
-    r = wepprun(row[0], row[2], row[3], row[4])
+    r = wepprun(row[0], row[2], row[3])
     r.run()
-    
+
 if __name__ == '__main__':
-    ''' Go main Go '''
+    # Go main Go
     realtime_run()
     pool = Pool() # defaults to cpu-count
     sts = datetime.datetime.now()
     sz = len(QUEUE)
-    for i,_ in enumerate(pool.imap_unordered(run, QUEUE),1):
+    for i, _ in enumerate(pool.imap_unordered(run, QUEUE), 1):
         if i > 0 and i % 5000 == 0:
             delta = datetime.datetime.now() - sts
             secs = delta.microseconds / 1000000. + delta.seconds
             speed = i / secs
             remaining = ((sz - i) / speed) / 3600.
             print ('%5.2fh Processed %6s/%6s [%.2f runs per sec] '
-                   +'remaining: %5.2fh') % (secs /3600., i, sz, speed, 
-                                            remaining )
-
-    
-    
+                   +'remaining: %5.2fh') % (secs /3600., i, sz, speed,
+                                            remaining)
