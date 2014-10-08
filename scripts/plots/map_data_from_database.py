3c781374644dff66561830049d66a8e1764b0ce0
akrherz <akrherz@iastate.edu>
2014-10-08 11:07:19 -0500

    sundry

--- scripts/plots/map_data_from_database.py
+++ scripts/plots/map_data_from_database.py
@@ -14,29 +14,36 @@ import matplotlib.cm as cm
 DBCONN = psycopg2.connect(database='idep')
 cursor = DBCONN.cursor()
 
-orig = {}
-for line in open('/tmp/flowpathcounts.txt'):
-    tokens = line.split()
-    orig[tokens[0]] = float(tokens[1])
 
 m = MapPlot(sector='iowa', axisbg='white', nologo=True,
             #subtitle='IDEPv2 minus IDEPv1',
-            title='IDEPv2 %s of Valid Flowpaths vs Gelder Provided by HUC12'  )
+            title='IDEPv2 Average Length Weighted Slope Change [%] by township'  )
 
+data = {}
+for line in open('tmp.txt'):
+    tokens = line.split(",")
+    data[ tokens[0] ] = float(tokens[1])
+
+data1 = {}
+for line in open('tmp1.txt'):
+    tokens = line.split(",")
+    data1[ tokens[0] ] = float(tokens[1]) * 100.
 
 
 cursor.execute("""
- WITH data as (
-  select huc_12, count(*) from flowpaths where scenario = 0 
-  GROUP by huc_12
-  )
+-- WITH data as (
+--  select model_twp, sum(slope * length) / sum(length) as val from 
+-- flowpath_points f, iatwp i where ST_Contains(i.the_geom, f.geom) 
+-- GROUP by model_twp
+-- )
 
- SELECT ST_Transform(geom, 4326), d.huc_12, d.count from 
- ia_huc12 h JOIN data d on (d.huc_12 = h.huc_12) ORDER by count DESC
+-- SELECT ST_Transform(the_geom, 4326), data.val * 100.0, data.model_twp from iatwp i JOIN data on (data.model_twp = i.model_twp)
+
+SELECT ST_Transform(the_geom, 4326), model_twp from iatwp i
 
 """)
 
-bins = np.arange(80,101,5)
+bins = np.arange(0,18,2)
 #cmap = cm.get_cmap('BrBG')
 #cmap.set_under('orange')
 #cmap.set_over('black')
@@ -52,16 +59,17 @@ for row in cursor:
         a = np.asarray(polygon.exterior)
         x,y = m.map(a[:,0], a[:,1])
         a = zip(x,y)
-        percentage = row[2] / orig[row[1]] * 100.0
-        if percentage < 90:
-            print "%s %.1f %s/%s" % (row[1], percentage, row[2], orig[row[1]])
-        c = cmap( norm([percentage,] ))[0]
+        #c = cmap( norm([float(row[1]),]) )[0]
+        diff = data.get(row[1], 0) - data1.get(row[1], 0)
+        print '%10s 1:%5.2f 2:%5.2f' % (row[1],data1.get(row[1], 0), data.get(row[1], 0))
+        diff = data.get(row[1], 0)
+        c = cmap( norm([float( diff),] ))[0]
         p = Polygon(a,fc=c,ec='None',zorder=2, lw=.1)
         patches.append(p)
 
           
 m.ax.add_collection(PatchCollection(patches,match_original=True))
-m.draw_colorbar(bins, cmap, norm, units='%s')
+m.draw_colorbar(bins, cmap, norm, units='%')
 
 m.drawcounties()
 m.postprocess(filename='test.png')
---
+++ scripts/plots/print_dbf_flowpath_counts.py
@@ -0,0 +1,31 @@
+"""
+Print out what the initial flowpath counts where 
+"""
+import dbflib
+import os
+import glob
+import pandas as pd 
+from pandas import Series
+
+def get_data(fn):
+    ''' Load a DBF file into a pandas DF '''
+    rows = []
+    dbf = dbflib.open(fn)
+    for i in range(dbf.record_count()):
+        rows.append( dbf.read_record(i) )
+    
+    return pd.DataFrame(rows)
+
+def main():
+    """ Lets go main go """
+    os.chdir("../../data/dbfsGo4")
+    fns = glob.glob("*.dbf")
+    for fn in fns:
+        huc12 = fn[4:-4]
+        huc8 = huc12[:-4]
+        df = get_data(fn)
+        flowpaths = Series(df['g4%s' % (huc8,)]).unique()
+        print huc12, len(flowpaths)
+    
+if __name__ == '__main__':
+    main()
