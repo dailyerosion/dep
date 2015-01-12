b15378f08847447d20ffd5048b7aaeb13a1be39b
akrherz <akrherz@iastate.edu>
2015-01-12 12:41:02 -0600

    Add custom extraction of yearly summaries

---
+++ scripts/util/export_erosion_summaries.py
@@ -0,0 +1,13 @@
+"""
+ Create a Shapefile with yearly erosion summarized by huc12
+"""
+import subprocess
+
+
+def do(year):
+    """ Process this year """
+    subprocess.call(""" pgsql2shp -f idep%s.shp -h iemdb -u nobody -g geom idep  "select i.geom, r.huc_12, sum(avg_loss) as loss  from results_by_huc12 r JOIN ia_huc12 i on (i.huc_12 = r.huc_12)  where valid between '%s-01-01' and '%s-01-01'  GROUP by r.huc_12, i.geom" """ % (year, year, year+1), shell=True)
+
+
+for yr in range(2007,2014):
+    do(yr)
