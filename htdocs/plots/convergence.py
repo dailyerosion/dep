988398d3661addde2bbb5f0da4c5b3339f853b59
akrherz <akrherz@iastate.edu>
2015-01-15 12:16:45 -0600

    Add stddev and 2sigma population

--- htdocs/plots/convergence.py
+++ htdocs/plots/convergence.py
@@ -37,7 +37,12 @@ def make_plot(huc_12):
             x.append(i)
             y.append(np.average(data[yr][:i]))
         
-        ax.plot(x, y, label='%i %.1f' % (yr, y[-1]))
+        stddev = np.std(data[yr])
+        mu = y[-1]
+        cnt = np.sum(np.where(data[yr]>(mu+2.*stddev), 1, 0))
+        ax.plot(x, y, label="%i $\mu$=%.1f\n$\sigma$=%.1f <$2\sigma$%%=%.1f" % (
+                            yr, mu, stddev, 
+                            (len(data[yr])-cnt)/float(len(data[yr]))*100.))
 
     ax.set_title("Soil Detachment Convergence for HUC12: %s" % (huc_12))
     ax.set_ylabel("Average Soil Detachment [tons/acre]")
@@ -46,11 +51,11 @@ def make_plot(huc_12):
     
     # Shrink current axis's height by 10% on the bottom
     box = ax.get_position()
-    ax.set_position([box.x0, box.y0 + box.height * 0.2,
-                 box.width, box.height * 0.8])
+    ax.set_position([box.x0, box.y0 + box.height * 0.35,
+                 box.width, box.height * 0.65])
 
-    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1),
-          fancybox=True, shadow=True, ncol=4, scatterpoints=1, fontsize=12)
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.13),
+          fancybox=True, shadow=True, ncol=3, scatterpoints=1, fontsize=10)
 
 
     # Sent output    
