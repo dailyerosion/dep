609b27736728ce17f7f05594a2a44ed1cb0a9fd2
akrherz <akrherz@iastate.edu>
2015-09-08 08:00:50 -0500

    Add example script that downloads DEP daily shapefiles

---
+++ scripts/util/dl_daily_shapefiles.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""Example script to download daily shapefiles from the dailyerosion site"""
+import datetime
+import urllib2
+
+
+start_time = datetime.date(2007, 1, 1)
+end_time = datetime.date(2015, 9, 8)
+interval = datetime.timedelta(days=1)
+
+now = start_time
+while now < end_time:
+    print("Downloading shapefile for %s" % (now.strftime("%d %b %Y"),))
+    uri = ("http://dailyerosion.org/dl/shapefile.py?dt=%s"
+           ) % (now.strftime("%Y-%m-%d"), )
+    fn = "dep%s.zip" % (now.strftime("%Y%m%d"), )
+    o = open(fn, 'wb')
+    o.write(urllib2.urlopen(uri).read())
+    o.close()
+    now += interval
