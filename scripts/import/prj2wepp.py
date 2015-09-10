f31661e30940c8e867aecb224babecaa8d3a47bb
akrherz <akrherz@iastate.edu>
2015-09-10 10:09:39 -0500

    pep8 cleanups

--- scripts/import/prj2wepp.py
+++ scripts/import/prj2wepp.py
@@ -14,6 +14,7 @@ if __name__ == '__main__':
     # Go Main Go
     os.chdir("/i/%s/prj" % (SCENARIO,))
     huc8s = glob.glob("*")
+    # huc8s = ['10290101', ]
     sz = len(huc8s)
     for i, huc8 in enumerate(huc8s):
         print "%04i/%04i %s" % (i+1, sz, huc8)
@@ -23,28 +24,28 @@ if __name__ == '__main__':
             os.chdir(huc4)
             for pfile in glob.glob("*.prj"):
                 fullfn = "/i/%s/prj/%s/%s/%s" % (SCENARIO, huc8, huc4, pfile)
-                proc = subprocess.Popen("%s %s test %s no" % (
-                                                        EXE, fullfn, WEPP),
-                            shell=True, stderr=subprocess.PIPE,
-                            stdout=subprocess.PIPE)
+                proc = subprocess.Popen(("%s %s test %s no"
+                                         ) % (EXE, fullfn, WEPP),
+                                        shell=True, stderr=subprocess.PIPE,
+                                        stdout=subprocess.PIPE)
                 # Need to block the above
                 stdout = proc.stdout.read()
                 if not os.path.isfile('test.man'):
-                    print '---> ERROR generating output for %s%s\n%s' % (huc12,
-                                                    proc.stderr.read(),
-                                                    stdout)
+                    print(('---> ERROR generating output for %s%s\n%s'
+                           ) % (huc12, proc.stderr.read(), stdout))
                     sys.exit()
                 # This generates .cli, .man, .run, .slp, .sol
                 # We need the .man , .slp , .sol from this process
                 for suffix in ['man', 'slp', 'sol']:
-                    shutil.copyfile('test.%s' % (suffix,), 
-                                    "/i/%s/%s/%s/%s/%s.%s" % (SCENARIO,
-                                            suffix, huc8, huc4, 
-                                            pfile[:-4], suffix))
-                
+                    shutil.copyfile('test.%s' % (suffix,),
+                                    ("/i/%s/%s/%s/%s/%s.%s"
+                                     ) % (SCENARIO,
+                                          suffix, huc8, huc4,
+                                          pfile[:-4], suffix))
+
                 for suffix in ['cli', 'man', 'run', 'slp', 'sol']:
                     if os.path.isfile("test.%s" % (suffix,)):
                         os.unlink("test.%s" % (suffix,))
-                    
+
             os.chdir("..")
         os.chdir("..")
