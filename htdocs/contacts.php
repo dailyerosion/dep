bea7b0afd42861a8fef1be8cd9d22da7f9ea7ae1
akrherz <akrherz@iastate.edu>
2016-04-20 09:27:40 -0500

    add initial placeholder for contacts

--- htdocs/contacts.php
+++ htdocs/contacts.php
@@ -7,7 +7,14 @@ $t = new MyView();
 $t->title = 'Project Contacts';
 
 $t->content = <<<EOF
-<h3>Project Contacts...</h3>
+<h3>Project Contacts</h3>
+
+<p><b>Principal Investigator:</b> Dr Rick Cruse  
+<br /><i>Email:</i>  <a href="mailto:rmc@iastate.edu">rmc@iastate.edu</a></p>
+
+<p><b>Technical/Webmaster:</b> Daryl Herzmann
+<br /><i>Email:</i> <a href="mailto:akrherz@iastate.edu">akrherz@iastate.edu</a></p>
+
 EOF;
 
 $t->render('single.phtml');
