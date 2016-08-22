57f2d62c6ac00ff7a39a1b861b85db00186af3fa
akrherz <akrherz@iastate.edu>
2016-08-22 09:24:17 -0500

    more diagnostics to UCS

--- scripts/ucs/results.py
+++ scripts/ucs/results.py
@@ -1,26 +1,28 @@
 import psycopg2
 import numpy as np
+import sys
 import matplotlib.pyplot as plt
 from pandas.io.sql import read_sql
 
 years = 8.0
+scenario = int(sys.argv[1])
 pgconn = psycopg2.connect(database='idep', host='iemdb', user='nobody')
 
 df = read_sql("""SELECT r.huc_12, r.scenario,
     sum(avg_loss) * 4.463 as detach
     from results_by_huc12 r
     , huc12 h WHERE r.huc_12 = h.huc_12 and h.states ~* 'IA'
-    and r.scenario in (0, 7) and h.scenario = 0 and r.valid < '2016-01-01'
+    and r.scenario in (0, %s) and h.scenario = 0 and r.valid < '2016-01-01'
     and r.valid > '2008-01-01'
     GROUP by r.scenario, r.huc_12
     ORDER by detach DESC
-    """, pgconn, index_col=None)
+    """, pgconn, params=(scenario,), index_col=None)
 
 s0 = df[df['scenario'] == 0].copy()
 s0.set_index('huc_12', inplace=True)
 s0.rename(columns={'detach': 'detach_s0', 'scenario': 'scenario_s0'},
           inplace=True)
-s7 = df[df['scenario'] == 7].copy()
+s7 = df[df['scenario'] == scenario].copy()
 s7.set_index('huc_12', inplace=True)
 s7.rename(columns={'detach': 'detach_s7', 'scenario': 'scenario_s7'},
           inplace=True)
@@ -31,11 +33,21 @@ df.sort_values(by='detach_s0', ascending=False, inplace=True)
 d0 = df['detach_s0'].values
 d7 = df['detach_s7'].values
 
-x = np.arange(0, len(df.index))
+sz = len(df.index)
+x = np.arange(0, sz)
 y = []
-for i in range(0, len(df.index)):
+key = sz / 4
+minval = [0, 1000.]
+for i in range(0, sz):
     d0[i] = d7[i]
-    y.append(np.average(d0) / years)
+    thisy = np.average(d0) / years
+    y.append(thisy)
+    if thisy < minval[1]:
+        minval = [i, thisy]
+    if i == key:
+        print y[0], y[i], (y[0] - y[i]) / y[0] * 100.
+
+print minval[0] / float(sz) * 100., (y[0] - minval[1]) / y[0] * 100.
 
 (fig, ax) = plt.subplots(1, 1)
 
