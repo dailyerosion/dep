0fec4e5768bd9628668715ff6e4cb900c9a35b82
akrherz <akrherz@iastate.edu>
2016-09-16 10:20:48 -0500

    add web service docs page

--- htdocs/geojson/huc12.py
+++ htdocs/geojson/huc12.py
@@ -31,7 +31,7 @@ def myjenks(array, label):
     return [float(_) for _ in a]
 
 
-def do(ts, ts2):
+def do(ts, ts2, domain):
     """Do work"""
     pgconn = psycopg2.connect(database='idep', host='iemdb', user='nobody')
     cursor = pgconn.cursor()
@@ -41,10 +41,13 @@ def do(ts, ts2):
     if ts2 is not None:
         dextra = "valid >= %s and valid <= %s"
         args = (ts, ts2)
+    domainextra = ''
+    if domain is not None:
+        domainextra = " and states ~* '%s'" % (domain[:2].upper(),)
     cursor.execute("""WITH data as (
         SELECT ST_asGeoJson(ST_Transform(simple_geom, 4326), 4) as g,
         huc_12
-        from huc12 WHERE scenario = 0), obs as (
+        from huc12 WHERE scenario = 0 """ + domainextra + """), obs as (
         SELECT huc_12,
             sum(coalesce(avg_loss, 0)) * 4.463 as avg_loss,
             sum(coalesce(avg_delivery, 0)) * 4.463 as avg_delivery,
@@ -105,19 +108,21 @@ def main(argv):
     sys.stdout.write("Content-Type: application/vnd.geo+json\n\n")
     form = cgi.FieldStorage()
     cb = form.getfirst('callback', None)
+    domain = form.getfirst('domain', None)
     ts = datetime.datetime.strptime(form.getfirst('date', '2015-05-05'),
                                     '%Y-%m-%d')
     ts2 = None
     if form.getfirst('date2', None) is not None:
         ts2 = datetime.datetime.strptime(form.getfirst('date2'), '%Y-%m-%d')
 
-    mckey = ("/geojson/huc12/%s/%s"
+    mckey = ("/geojson/huc12/%s/%s/%s"
              ) % (ts.strftime("%Y%m%d"),
-                  '' if ts2 is None else ts2.strftime("%Y%m%d"))
+                  '' if ts2 is None else ts2.strftime("%Y%m%d"),
+                  '' if domain is None else domain)
     mc = memcache.Client(['iem-memcached:11211'], debug=0)
     res = mc.get(mckey)
     if not res:
-        res = do(ts, ts2)
+        res = do(ts, ts2, domain)
         mc.set(mckey, res, 15)
 
     if cb is None:
