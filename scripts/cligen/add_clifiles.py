9d1b28a1e26a72e7272f581d64c778e48ab22302
akrherz <akrherz@iastate.edu>
2017-01-24 21:45:53 -0600

    change add_clifiles logic to look at 0.25 nodes

--- scripts/cligen/add_clifiles.py
+++ scripts/cligen/add_clifiles.py
@@ -13,19 +13,16 @@ def missing_logic(scenario, fn):
     lat = float(fn[24:30])
     if not os.path.isdir(os.path.dirname(fn)):
         os.makedirs(os.path.dirname(fn))
-    for xoff in [0, -1, 1, -2, 2, -3, 3]:
-        for yoff in [0, -1, 1, -2, 2, -3, 3]:
-            lon2 = lon + xoff / 100.0
-            lat2 = lat + yoff / 100.0
-            testfn = ("/i/%s/cli/%03.0fx%03.0f/%06.2fx%06.2f.cli"
-                      ) % (scenario, lon2, lat2, lon2, lat2)
-            if not os.path.isfile(testfn):
-                continue
-            print("%s->%s" % (testfn, fn))
-            shutil.copyfile(testfn, fn)
-            return
-    print("--> failure for %s, using default file" % (fn,))
-    shutil.copyfile("/i/0/cli/092x041/092.24x040.71.cli", fn)
+    # So there should be a file at an interval of 0.25
+    lon2 = lon - (lon * 100 % 25) / 100.
+    lat2 = lat - (lat * 100 % 25) / 100.
+    testfn = ("/i/%s/cli/%03.0fx%03.0f/%06.2fx%06.2f.cli"
+              ) % (scenario, lon2, lat2, lon2, lat2)
+    if not os.path.isfile(testfn):
+        print("Whoa, why doesn't %s exist?" % (testfn,))
+        sys.exit()
+    print("%s->%s" % (testfn, fn))
+    shutil.copyfile(testfn, fn)
 
 
 def main(argv):
