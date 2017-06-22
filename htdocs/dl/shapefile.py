9d53f1aedfd3a86b681cf836fbf6864c3a7c7e67
akrherz <akrherz@iastate.edu>
2017-06-21 22:17:15 -0500

    fix shapefile download

--- htdocs/dl/shapefile.py
+++ htdocs/dl/shapefile.py
@@ -6,72 +6,49 @@ import cgi
 import datetime
 import sys
 import os
-import shapelib
-import dbflib
 import shutil
 import zipfile
-from pyiem import wellknowntext
-import psycopg2.extras
+
+import psycopg2
+from geopandas import GeoDataFrame
 
 
-def do(dt, dt2):
+def workflow(dt, dt2):
     """Generate for a given date """
     dbconn = psycopg2.connect(database='idep', host='iemdb', user='nobody')
-    cursor = dbconn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     dextra = "valid = %s"
     args = (dt,)
     if dt2 is not None:
         dextra = "valid >= %s and valid <= %s"
         args = (dt, dt2)
-    cursor.execute("""WITH data as (
-        SELECT ST_AsText(geom) as g,
-        huc_12
-        from huc12 WHERE scenario = 0), obs as (
-        SELECT huc_12,
+    df = GeoDataFrame.from_postgis("""
+        WITH data as (
+            SELECT simple_geom, huc_12
+            from huc12 WHERE scenario = 0),
+        obs as (
+            SELECT huc_12,
             sum(coalesce(avg_loss, 0)) as avg_loss,
             sum(coalesce(avg_delivery, 0)) as avg_delivery,
             sum(coalesce(qc_precip, 0)) as qc_precip,
             sum(coalesce(avg_runoff, 0)) as avg_runoff
-        from results_by_huc12 WHERE
-        """+dextra+""" and scenario = 0 GROUP by huc_12)
+            from results_by_huc12 WHERE
+            """+dextra+""" and scenario = 0 GROUP by huc_12)
 
-        SELECT d.g, d.huc_12,
-        coalesce(o.avg_loss, 0),
-        coalesce(o.qc_precip, 0),
-        coalesce(o.avg_delivery, 0),
-        coalesce(o.avg_runoff, 0)
+        SELECT d.simple_geom, d.huc_12,
+        coalesce(o.qc_precip, 0) as prec_mm,
+        coalesce(o.avg_loss, 0) as los_kgm2,
+        coalesce(o.avg_runoff, 0) as runof_mm,
+        coalesce(o.avg_delivery, 0) as deli_kgm
         from data d LEFT JOIN obs o ON (d.huc_12 = o.huc_12)
-    """, args)
+    """, dbconn, params=args, geom_col='simple_geom')
 
     os.chdir("/tmp")
     fn = "idepv2_%s" % (dt.strftime("%Y%m%d"),)
     if dt2:
         fn += dt2.strftime("_%Y%m%d")
-    shp = shapelib.create(fn, shapelib.SHPT_POLYGON)
-
-    dbf = dbflib.create(fn)
-    dbf.add_field("HUC_12", dbflib.FTString, 12, 0)
-    dbf.add_field("PREC_MM", dbflib.FTDouble, 8, 2)
-    dbf.add_field("LOS_KGM2", dbflib.FTDouble, 8, 2)
-    dbf.add_field("RUNOF_MM", dbflib.FTDouble, 8, 2)
-    dbf.add_field("DELI_KGM", dbflib.FTDouble, 8, 2)
-
-    for i, row in enumerate(cursor):
-        g = wellknowntext.convert_well_known_text(row[0])
-        obj = shapelib.SHPObject(shapelib.SHPT_POLYGON, 1, g)
-        shp.write_object(-1, obj)
-        del(obj)
-
-        dbf.write_record(i, dict(HUC_12=row[1],
-                                 PREC_MM=row[3],
-                                 LOS_KGM2=row[2],
-                                 RUNOF_MM=row[5],
-                                 DELI_KGM=row[4]))
-
-    # hack way to close the files
-    del(shp)
-    del(dbf)
-
+    df.columns = [s.upper() if s != 'simple_geom' else s
+                  for s in df.columns.values]
+    df.to_file(fn + ".shp")
     shutil.copyfile("/opt/iem/data/gis/meta/5070.prj",
                     fn+".prj")
     z = zipfile.ZipFile(fn+".zip", 'w', zipfile.ZIP_DEFLATED)
@@ -98,7 +75,8 @@ def main():
     dt2 = form.getfirst('dt2')
     if dt2 is not None:
         dt2 = datetime.datetime.strptime(form.getfirst('dt2'), '%Y-%m-%d')
-    do(dt, dt2)
+    workflow(dt, dt2)
+
 
 if __name__ == '__main__':
     main()
