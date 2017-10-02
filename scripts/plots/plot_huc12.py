afed4799d64b7f9d635518091d8f4bb58128f173
akrherz <akrherz@iastate.edu>
2017-10-02 14:07:43 -0500

    plot related to #48 t management counts

--- scripts/plots/plot_huc12.py
+++ scripts/plots/plot_huc12.py
@@ -1,68 +1,61 @@
-'''
-Explore mapping IDEPv1 data direct from the database with matplotlib
-'''
+"""Go"""
+from __future__ import print_function
+
 from pyiem.plot import MapPlot, maue, james2, james
 from shapely.wkb import loads
 import psycopg2
-import sys
 import numpy as np
 from matplotlib.patches import Polygon
-from matplotlib.collections import PatchCollection
 import matplotlib.colors as mpcolors
-import matplotlib.cm as cm
-
-DBCONN = psycopg2.connect(database='idep')
-cursor = DBCONN.cursor()
-
-orig = {}
-for line in open('/tmp/flowpathcounts.txt'):
-    tokens = line.split()
-    orig[tokens[0]] = float(tokens[1])
-
-m = MapPlot(sector='iowa', axisbg='white', nologo=True,
-            #subtitle='IDEPv2 minus IDEPv1',
-            title='IDEPv2 %s of Valid Flowpaths vs Gelder Provided by HUC12'  )
+import cartopy.crs as ccrs
 
 
+def main():
+    pgconn = psycopg2.connect(database='idep')
+    cursor = pgconn.cursor()
 
-cursor.execute("""
- WITH data as (
-  select huc_12, count(*) from flowpaths where scenario = 0 
-  GROUP by huc_12
-  )
+    mp = MapPlot(continentalcolor='white', nologo=True, sector='custom',
+                 south=36.8, north=45.0, west=-99.2, east=-88.9,
+                 title=("HUC12 Frequency of Flowpaths with 1+ segment with "
+                        "'T' management"))
 
- SELECT ST_Transform(geom, 4326), d.huc_12, d.count from 
- huc12 h JOIN data d on (d.huc_12 = h.huc_12) WHERE h.scenario = 0
- ORDER by count DESC
+    cursor.execute("""
+    with data as (
+        select distinct flowpath from flowpath_points
+        where (lu2007 || lu2008 || lu2009 || lu2010 || lu2011 || lu2012
+        || lu2013 || lu2014 || lu2015 || lu2016 || lu2017) ~* 'T'
+        and scenario = 0),
+    agg as (select huc_12, count(*) from flowpaths f JOIn data d
+    on (f.fid = d.flowpath) GROUP by huc_12)
 
-""")
+    SELECT ST_Transform(geom, 4326), h.huc_12,
+    coalesce(a.count, 0) from huc12 h
+    LEFT JOIN agg a on (h.huc_12 = a.huc_12) WHERE h.scenario = 0
+    ORDER by count DESC
+    """)
 
-bins = np.arange(80,101,5)
-#cmap = cm.get_cmap('BrBG')
-#cmap.set_under('orange')
-#cmap.set_over('black')
-cmap = james()
-cmap.set_under('white')
-cmap.set_over('black')
-norm = mpcolors.BoundaryNorm(bins, cmap.N)
-patches = []
-for row in cursor:
-    #print "%s,%s" % (row[2], row[1])
-    geom = loads( row[0].decode('hex') )
-    for polygon in geom:
-        a = np.asarray(polygon.exterior)
-        x,y = m.map(a[:,0], a[:,1])
-        a = zip(x,y)
-        percentage = row[2] / orig[row[1]] * 100.0
-        if percentage < 90:
-            print "%s %.1f %s/%s" % (row[1], percentage, row[2], orig[row[1]])
-        c = cmap( norm([percentage,] ))[0]
-        p = Polygon(a,fc=c,ec='None',zorder=2, lw=.1)
-        patches.append(p)
+    bins = np.arange(1, 45, 4)
+    cmap = james2()
+    cmap.set_under('tan')
+    cmap.set_over('black')
+    norm = mpcolors.BoundaryNorm(bins, cmap.N)
+    for row in cursor:
+        if row[2] > 30:
+            print("%s %s" % (row[1], row[2]))
+        multipoly = loads(row[0].decode('hex'))
+        for polygon in multipoly:
+            arr = np.asarray(polygon.exterior)
+            points = mp.ax.projection.transform_points(ccrs.Geodetic(),
+                                                       arr[:, 0], arr[:, 1])
+            color = cmap(norm([float(row[2]), ]))[0]
+            poly = Polygon(points[:, :2], fc=color, ec='None', zorder=2, lw=.1)
+            mp.ax.add_patch(poly)
 
-          
-m.ax.add_collection(PatchCollection(patches,match_original=True))
-m.draw_colorbar(bins, cmap, norm, units='%s')
+    mp.draw_colorbar(bins, cmap, norm, units='count')
 
-m.drawcounties()
-m.postprocess(filename='test.png')
+    mp.drawcounties()
+    mp.postprocess(filename='test.png')
+
+
+if __name__ == '__main__':
+    main()
