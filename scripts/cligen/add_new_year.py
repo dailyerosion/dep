d5ec7608940e4ec01970ff15297b1ca95bb8f091
akrherz <akrherz@iastate.edu>
2017-12-06 08:56:50 -0600

    improve robustness of addition of year to CLI file

--- scripts/cligen/add_new_year.py
+++ scripts/cligen/add_new_year.py
@@ -1,8 +1,12 @@
 """Add additional year to the climate files
 
-Note that this does not adjust the header for years modeled. Why not?  Because
-we currently have it set to a large number and WEPP appears to ignore it.
+    python add_new_year.py <scenario> <year_to_add>
+
+The previous year is choosen based on if the year to add is a leap year or not,
+if it is, then pick a year four years ago, if it isn't, use last year
+
 """
+from __future__ import print_function
 import glob
 import sys
 import os
@@ -10,39 +14,40 @@ import subprocess
 from tqdm import tqdm
 
 
-def do(fn, year):
-    data = open(fn).read()
+def workflow(filename, year):
+    """Effort this file, please"""
+    lines = open(filename).readlines()
+    # Replace the header information denoting years simulated
+    years_simulated = (year - 2007) + 1
+    lines[4] = ("    41.53   -93.65         289          %i        2007"
+                "              %i\n"
+                ) % (years_simulated, years_simulated)
+    data = "".join(lines)
     analog = year - 1 if year % 4 != 0 else year - 4
     if data.find("1\t1\t%s" % (year, )) > 0:
-        print("%s already has %s data" % (fn, year))
+        print("%s already has %s data" % (filename, year))
         return
     pos = data.find("1\t1\t%s" % (analog, ))
     pos2 = data.find("1\t1\t%s" % (analog + 1,))
-    out = open("/tmp/%s" % (fn,), 'w')
-    extra = "\n" if data[-1] != "\n" else ''
-    out.write(data + extra + data[pos:pos2].replace(str(analog), str(year)) +
-              extra)
+    content = data[pos:pos2] if pos2 > 0 else data[pos:]
+    out = open("/tmp/%s" % (filename,), 'w')
+    # careful here to get the line feeds right
+    out.write(data + content.replace(str(analog), str(year)))
     out.close()
-    subprocess.call("mv /tmp/%s %s" % (fn, fn), shell=True)
+    subprocess.call("mv /tmp/%s %s" % (filename, filename), shell=True)
 
 
 def main(argv):
+    """Go Main Go"""
     scenario = argv[1]
     year = int(argv[2])
     os.chdir("/i/%s/cli" % (scenario,))
-    errors = 0
     for mydir in tqdm(glob.glob("*")):
-        if errors > 100:
-            print("Aborting with too many errors")
-            sys.exit()
         os.chdir(mydir)
         for fn in glob.glob("*.cli"):
-            try:
-                do(fn, year)
-            except Exception as exp:
-                print("%s resulted in exp: %s" % (fn, exp))
-                errors += 1
+            workflow(fn, year)
         os.chdir("..")
+
 
 if __name__ == "__main__":
     main(sys.argv)
