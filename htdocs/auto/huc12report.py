8ef5c48f6e3a3d11758868f79d1ca78dbff34fbd
akrherz <akrherz@iastate.edu>
2018-08-06 09:15:08 -0500

    add monthly summary #61

--- htdocs/auto/huc12report.py
+++ htdocs/auto/huc12report.py
@@ -3,6 +3,7 @@
 from io import BytesIO
 import datetime
 import cgi
+import calendar
 
 import requests
 from pandas.io.sql import read_sql
@@ -19,6 +20,49 @@ INTROTEXT = """Hello there! This is from glorious automation. Someday, you will
 find interesting things here and actual details on what fun this is trying to
 accomplish.  Until then, you must await more magic to happen.
 """
+
+
+def generate_monthly_summary_table(huc12):
+    """Make a table of monthly summary stats."""
+    data = []
+    data.append(['Year', 'Month', 'Precip', "Runoff", "Loss", "Delivery",
+                '2+" Precip', 'Events'])
+    data.append(['', '', '[inch]', "[inch]", "[tons/acre]", "[tons/acre]",
+                 "[days]", "[days]"])
+    pgconn = get_dbconn('idep')
+    df = read_sql("""
+        SELECT extract(year from valid)::int as year,
+        extract(month from valid)::int as month,
+        round((sum(qc_precip) / 25.4)::numeric, 2),
+        round((sum(avg_runoff) / 25.4)::numeric, 2),
+        round((sum(avg_loss) * 4.463)::numeric, 2),
+        round((sum(avg_delivery) * 4.463)::numeric, 2),
+        sum(case when qc_precip >= 50.8 then 1 else 0 end) as pdays,
+        sum(case when avg_loss > 0 then 1 else 0 end) as events
+        from results_by_huc12 WHERE scenario = 0 and huc_12 = %s
+        and valid >= '2016-01-01'
+        GROUP by year, month ORDER by year, month ASC
+    """, pgconn, params=(huc12, ), index_col=None)
+    for _, row in df.iterrows():
+        vals = [int(row['year']), calendar.month_abbr[int(row['month'])]]
+        vals.extend(["%.2f" % (f, ) for f in list(row)[2:-2]])
+        vals.extend(["%.0f" % (f, ) for f in list(row)[-2:]])
+        data.append(vals)
+    data[-1][1] = "%s*" % (data[-1][1], )
+    totals = df.iloc[:-1].mean()
+    vals = ['', 'Average']
+    vals.extend(["%.2f" % (f, ) for f in list(totals[2:])])
+    data.append(vals)
+
+    style = TableStyle(
+        [('LINEBELOW', (2, 1), (-1, 1), 0.5, '#000000'),
+         ('LINEAFTER', (1, 2), (1, -2), 0.5, '#000000'),
+         ('LINEABOVE', (2, -1), (-1, -1), 0.5, '#000000'),
+         ('ALIGN', (0, 0), (-1, -1), 'RIGHT')]
+    )
+    for rownum in range(3, len(data)+1, 2):
+        style.add('LINEBELOW', (0, rownum), (-1, rownum), 0.25, '#EEEEEE')
+    return Table(data, style=style)
 
 
 def generate_summary_table(huc12):
@@ -123,7 +167,9 @@ def main():
                             '(* year to date total)'
                             ), styles['Normal']))
     story.append(PageBreak())
-    story.append(Paragraph('Content to be determined....', styles['Normal']))
+    story.append(Spacer(inch, inch * 0.25))
+    story.append(Paragraph('Monthly Summary', styles['Heading1']))
+    story.append(generate_monthly_summary_table(huc12))
 
     def pagecb(canvas, doc):
         """Proxy to our draw_header func"""
