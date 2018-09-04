1fd4203a64298165f97a047cc7d6589b8637448c
akrherz <akrherz@iastate.edu>
2018-09-04 11:57:21 -0500

    create workflow script for HUD reports (#61)

--- scripts/hud/deliver_reports.py
+++ scripts/hud/deliver_reports.py
@@ -1,29 +1,85 @@
-"""Generate and upload DEP reports.
-Clear Creek
-Dubuque and Bee Branch Creek
-East Nishnabotna River
-English River
-Middle Cedar River
-North Raccoon River
-Upper Iowa River
-Upper Wapsipinicon River
-West Nishnabotna River
+"""Generate and upload DEP reports."""
 
-Headwaters Cedar Creek	071000060202
-Outlet Creek	071000060307
-Bear Creek-Cedar River	070802051004
-Lime Creek	070802051003
-Hinkle Creek	070802051102
-Mud Creek	070802050907
-Opossum Creek	070802051201
-Wildcat Creek	070802051202
-Devils Run-Wolf Creek	070802050808
-Twelvemile Creek	070802050807
-Headwaters North English	070802090401
-Upper Clear Creek	070802090101
-Middle Clear Creek	070802090102
-Lower Clear Creek	070802090103
-Middle North English River	070802090406
-Middle English River	070802090302
-Gritter Creek	070802090301
-"""
+from pandas.io.sql import read_sql
+import geopandas as gpd
+import requests
+from pyiem.util import get_dbconn
+from pyiem.box_utils import sendfiles2box
+
+LOOKUP = {
+    "10240003": "East Nishnabotna River",
+    "07080205": "Middle Cedar River",
+    "07100006": "North Raccoon River",
+    "07060002": "Upper Iowa River",
+    "07080102": "Upper Wapsipinicon River",
+    "10240002": "West Nishnabotna River",
+}
+
+
+def build_xref():
+    """Figure out which HUC12s we need and where to file them."""
+    xref = {}
+    # exporthucs can use the LOOKUP
+    df = gpd.read_file('exporthucs.shp')
+    for _i, row in df.iterrows():
+        huc8 = row['HUC_12'][:8]
+        # A misfire of some HUCs in South Dakota
+        if huc8 not in LOOKUP:
+            continue
+        refs = xref.setdefault(LOOKUP[huc8], [])
+        refs.append(row['HUC_12'])
+
+    # English River
+    df = gpd.read_file('english.shp')
+    for _i, row in df.iterrows():
+        refs = xref.setdefault('English River', [])
+        refs.append(row['HUC_12'])
+
+    # Clear Creek
+    xref['Clear Creek'] = ['070802090101', '070802090102', '070802090103']
+    # TODO Dubuque and Bee Branch Creek
+    return xref
+
+
+def workflow(name, hucs):
+    """Do our processing please."""
+    # Construct the lookup table
+    pgconn = get_dbconn('idep')
+    df = read_sql("""
+    select huc_12, hu_12_name from huc12 where scenario = 0
+    and huc_12 in %s
+    """, pgconn, params=(tuple(hucs), ), index_col='huc_12')
+    filenames = []
+    for huc in hucs:
+        if huc not in df.index.values:
+            print("Huc: %s is unknown to DEP" % (huc, ))
+            continue
+        uri = "http://dailyerosion.local/auto/huc12report.py?huc=%s" % (huc,)
+        req = requests.get(uri)
+        if req.status_code != 200:
+            print("Uri: %s failed with: %s" % (uri, req.status_code))
+            continue
+        fn = "%s-%s.pdf" % (huc, df.at[huc, 'hu_12_name'].replace(" ", "_"))
+        fp = open(fn, 'wb')
+        fp.write(req.content)
+        fp.close()
+        filenames.append(fn)
+
+    res = sendfiles2box(
+        "Iowa Watershed Approach - Daily Erosion Project/%s" % (name, ),
+        filenames
+    )
+    for retval, fn in zip(res, filenames):
+        if retval is None:
+            print("%s failed to upload." % (fn, ))
+
+
+def main():
+    """Go Main Go."""
+    xref = build_xref()
+    for name, hucs in xref.items():
+        workflow(name, hucs)
+
+
+if __name__ == '__main__':
+    main()
