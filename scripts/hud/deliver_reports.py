2556671ae46432433416842094c9fe34952723f6
akrherz <akrherz@iastate.edu>
2018-09-20 11:13:34 -0500

    add excel output report as per #61

--- scripts/hud/deliver_reports.py
+++ scripts/hud/deliver_reports.py
@@ -1,6 +1,7 @@
 """Generate and upload DEP reports."""
 
 from pandas.io.sql import read_sql
+import pandas as pd
 import geopandas as gpd
 import requests
 from pyiem.util import get_dbconn
@@ -41,7 +42,37 @@ def build_xref():
     return xref
 
 
-def workflow(name, hucs):
+def excel_summary(hucs, name):
+    """Generate a Excel file with yearly summaries."""
+    pgconn = get_dbconn('idep')
+    df = read_sql("""
+        SELECT huc_12, extract(year from valid) as year,
+        sum(avg_loss) * 4.463 as loss_ton_per_acre,
+        sum(avg_delivery) * 4.463 as delivery_ton_per_acre,
+        sum(qc_precip) / 25.4 as precip_inch,
+        sum(avg_runoff) / 25.4 as runoff_inch
+        from results_by_huc12 WHERE
+        scenario = 0 and huc_12 in %s and valid >= '2007-01-01'
+        and valid < '2018-01-01' GROUP by huc_12, year
+    """, pgconn, params=(tuple(hucs), ))
+    writer = pd.ExcelWriter(
+        '%s.xlsx' % (name, ), options={'remove_timezone': True})
+    df.to_excel(writer, 'Yearly Totals', index=False)
+    gdf = df.groupby('huc_12').mean()
+    gdf[['loss_ton_per_acre', 'delivery_ton_per_acre', 'precip_inch',
+         'runoff_inch']].to_excel(writer, 'Yearly Averages')
+    format1 = writer.book.add_format({'num_format': '0.00'})
+    worksheet = writer.sheets['Yearly Totals']
+    worksheet.set_column('A:A', 18)
+    worksheet.set_column('C:F', 20, format1)
+    worksheet = writer.sheets['Yearly Averages']
+    worksheet.set_column('A:A', 18)
+    worksheet.set_column('B:E', 20, format1)
+
+    writer.save()
+
+
+def workflow(hucs):
     """Do our processing please."""
     # Construct the lookup table
     pgconn = get_dbconn('idep')
@@ -64,7 +95,11 @@ def workflow(name, hucs):
         fp.write(req.content)
         fp.close()
         filenames.append(fn)
+    return filenames
 
+
+def upload(filenames, name):
+    """Send our files to cybox."""
     res = sendfiles2box(
         "Iowa Watershed Approach - Daily Erosion Project/%s" % (name, ),
         filenames, overwrite=True
@@ -78,7 +113,11 @@ def main():
     """Go Main Go."""
     xref = build_xref()
     for name, hucs in xref.items():
-        workflow(name, hucs)
+        fns = []
+        # fns = workflow(hucs)
+        excel_summary(hucs, name)
+        fns.append("%s.xlsx" % (name, ))
+        upload(fns, name)
 
 
 if __name__ == '__main__':
