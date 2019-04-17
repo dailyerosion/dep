06e4a95079de26e149314d2cd4a5e373c213ba6b
akrherz <akrherz@iastate.edu>
2019-04-17 08:19:11 -0500

    use scenario info to drive make_dirs

--- scripts/util/make_dirs.py
+++ scripts/util/make_dirs.py
@@ -3,34 +3,33 @@ import os
 import sys
 
 from pyiem.util import get_dbconn
-
-SCENARIO = sys.argv[1]
-BASELINESCENARIO = 0 if len(sys.argv) == 2 else sys.argv[2]
-
+from pyiem.dep import load_scenarios
 
 PREFIXES = 'crop env man prj run slp sol wb error ofe yld'.split()
 
 
-def do_huc12(huc12):
+def do_huc12(scenario, huc12):
     """ Directory creator! """
     for prefix in PREFIXES:
-        newdir = "/i/%s/%s/%s/%s" % (SCENARIO, prefix, huc12[:8], huc12[8:])
+        newdir = "/i/%s/%s/%s/%s" % (scenario, prefix, huc12[:8], huc12[8:])
         if not os.path.isdir(newdir):
             os.makedirs(newdir)
 
 
-def main():
+def main(argv):
     """Do Main"""
+    scenario = int(argv[1])
+    sdf = load_scenarios()
     # Go Main Go
     pgconn = get_dbconn('idep')
     cursor = pgconn.cursor()
     cursor.execute("""
         SELECT distinct huc_12 from flowpaths
         WHERE scenario = %s
-    """, (BASELINESCENARIO,))
+    """, (int(sdf.at[scenario, 'huc12_scenario']),))
     for row in cursor:
-        do_huc12(row[0])
+        do_huc12(scenario, row[0])
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
