93c6c4fa2f2b2782f511f93c45def012b1a31d80
akrherz <akrherz@iastate.edu>
2019-08-16 22:03:37 -0500

    correct bad algo for locating new climate files

--- scripts/cligen/locate_clifile.py
+++ scripts/cligen/locate_clifile.py
@@ -1,3 +1,4 @@
+"""Find an available precip file after we do expansion."""
 import os
 import sys
 
@@ -6,14 +7,22 @@ from pyiem.dep import get_cli_fname
 
 
 def finder(lon, lat):
-    for deltax in range(25):
-       for multi in [-1 * deltax, 1 * deltax]:
-           for multi2 in [-1 * deltax, 1 * deltax]:
-               newlon = lon - multi * deltax / 100.
-               newlat = lat - multi2 * deltax / 100.
-               newfn = get_cli_fname(newlon, newlat)
-               if os.path.isfile(newfn):
-                   return newfn
+    """The logic of finding a file."""
+    # https://stackoverflow.com/questions/398299/looping-in-a-spiral
+    x = y = 0
+    dx = 0
+    dy = -1
+    X = 26
+    Y = 26
+    for _ in range(26**2):
+        if (-X/2 < x <= X/2) and (-Y/2 < y <= Y/2):
+            newfn = get_cli_fname(lon + x * 0.01, lat + y * 0.01)
+            if os.path.isfile(newfn):
+                return newfn, x, y
+        if x == y or (x < 0 and x == -y) or (x > 0 and x == 1-y):
+            dx, dy = -dy, dx
+        x, y = x+dx, y+dy
+    return None, None, None
 
 
 def main():
@@ -33,13 +42,19 @@ def main():
         lon, lat = fn.split("/")[-1][:-4].split("x")
         lon = 0 - float(lon)
         lat = float(lat)
-        newfn = finder(lon, lat)
+        newfn, xoff, yoff = finder(lon, lat)
         if newfn is None:
             print("missing %s for fid: %s " % (fn, row[1]))
             sys.exit()
-        print("%s -> %s" % (newfn, fn))
+        print("%s -> %s xoff: %s yoff: %s" % (newfn, fn, xoff, yoff))
         os.system("cp %s %s" % (newfn, fn))
     print("added %s files" % (created, ))
+
+
+def test_finder():
+    """Test what our finder does."""
+    finder(-95., 42.)
+    assert False
 
 
 if __name__ == '__main__':
