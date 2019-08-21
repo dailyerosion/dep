4b278e8479e293dfb5812d2063b17f85481434a1
akrherz <akrherz@iastate.edu>
2019-08-21 10:24:25 -0500

    sync laptop changes

--- scripts/import/README.md
+++ scripts/import/README.md
@@ -1,3 +1,5 @@
+Gelder Data Import Procedure
+============================
 
 Brian Gelder provides me a 7zip file with individual flowpaths included.  There
 is one file per HUC12.
@@ -6,20 +8,26 @@ is one file per HUC12.
 1. `python clear_files.py <scenario>`
 1. go to ../mangen and run `python build_management.py <scenario>`
 1. go to ../util and run `python make_dirs.py <scenario>`
-1. `python flowpath2prj.py <scenario>`
+1. cd to ../import and run `python flowpath2prj.py <scenario>`
 1. `python prj2wepp.py <scenario>`
 1. `python package_myhucs.py <scenario>`
 1. go to ../cligen and run `python assign_climate_file.py <scenario>`
 1. If new HUC12s are present, get an updated simplified HUC12 from Dave.
-1. Copy laptop database tables `flowpaths` and `flowpath_points` to IEMDB
+1. Copy laptop database tables `huc12`, `flowpaths` and `flowpath_points` to IEMDB
 1. copy `myhucs.txt` up to IEM and run `python clear_files.py`
 1. extract the `dep.tar` file on IEM
 1. On IEM run `cligen/locate_clifile.py`
 1. On IEM run `cligen/make_dirs.py`
 
- insert into huc12
- (states, hu_12_name, huc_8, huc_12, simple_geom, geom, scenario)
- select states, name, huc_8, huc_12, st_geometryn(geom, 1),
- geom, 0 from p200 where huc_12 in
- 	(select distinct huc_12 from flowpaths where huc_12 not in
- 		(select huc_12 from huc12 where scenario = 0) and scenario = 0);
+This query finds any new HUC12s and inserts the geometry into a table.
+
+    insert into huc12
+    (states, hu_12_name, huc_8, huc_12, simple_geom, geom, scenario)
+    select states, name, huc_8, huc_12, st_geometryn(geom, 1),
+    geom, 0 from p200 where huc_12 in
+    (select distinct huc_12 from flowpaths where huc_12 not in
+    (select huc_12 from huc12 where scenario = 0) and scenario = 0);
+
+We should also check that we don't have unknown tables.
+
+    select distinct huc_12 from flowpaths where scenario = 0 and huc_12 not in (select huc_12 from huc12 where scenario = 0) ORDER by huc_12;
--- scripts/plots/plot_wb.py
+++ scripts/plots/plot_wb.py
@@ -1,34 +1,105 @@
 """Diagnose some Water Balance stuff"""
 from __future__ import print_function
 import sys
-import calendar
+import glob
+import datetime
 
+from tqdm import tqdm
+import pandas as pd
+import seaborn as sns
+from pyiem.plot.use_agg import plt
 from pyiem.dep import read_wb
-import matplotlib.pyplot as plt
 
 
 def main(argv):
     """Do things"""
-    filename = argv[1]
-    wb = read_wb(filename)
+    dfs = []
+    for fn in glob.glob("/i/0/wb/07100004/0704/*"):
+        df = read_wb(fn)
+        df['fpath'] = int(fn.split("_")[1][:-3])
+        dfs.append(df)
+    df = pd.concat(dfs)
+    ranges = df.groupby(['fpath', 'ofe']).describe()
+    year = 2018
+    for doy in tqdm(range(1, 365)):
+        date = datetime.date(year, 1, 1) + datetime.timedelta(days=(doy - 1))
+        wb = df[(df['year'] == year) & (df['jday'] == doy)].copy()
+        wb = wb.set_index(['fpath', 'ofe'])
+        for f2 in ['sw1', 'sw2', 'sw']:
+            for f1 in ['min', 'max']:
+                wb['%s_%s' % (f2, f1)] = ranges[f2, f1]
+            wb['%s_range' % (f2, )] = (
+                wb['%s_max' % (f2, )] - wb['%s_min' % (f2, )]
+            )
+            wb['%s_percent' % (f2, )] = (
+                (wb[f2] - wb['%s_min' % (f2, )]) /
+                wb['%s_range' % (f2, )] * 100.
+            )
 
-    (fig, ax) = plt.subplots(2, 1)
-    ax[0].set_title("Water Balance for %s" % (filename.split("/")[-1], ))
-    ax[0].plot(wb['date'], wb['sw'])
-    ax[0].set_ylabel("Total Soil Water [mm]")
-    ax[0].grid(True)
+        sns.set(style="white", palette="muted", color_codes=True)
+        (fig, ax) = plt.subplots(3, 2, figsize=(7, 7))
+        sns.despine(left=True)
+        fig.text(
+            0.5, 0.98,
+            "%s :: Water Balance for 071000040704" % (
+                date.strftime("%d %B %Y"), ),
+            ha='center')
 
-    for year in [2009, 2012, 2015]:
-        df2 = wb[wb['year'] == year]
-        ax[1].plot(df2['jday'], df2['sw'], label=str(year))
-    ax[1].legend(ncol=10)
-    ax[1].set_xticks((1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305,
-                      335, 365))
-    ax[1].set_xticklabels(calendar.month_abbr[1:])
-    ax[1].grid(True)
-    ax[1].set_ylabel("Total Soil Water [mm]")
+        # ---------------------------------------------------
+        myax = ax[0, 0]
+        sns.distplot(
+            wb['sw1'], hist=False, color="g", ax=myax,
+            rug=True)
+        myax.set_xlabel('0-10cm Soil Water [mm]')
+        myax.axvline(wb['sw1'].mean(), color='r')
+        myax.set_xlim(0, 60)
 
-    fig.savefig('test.png')
+        myax = ax[0, 1]
+        sns.distplot(
+            wb['sw1_percent'], hist=False, color="g", ax=myax,
+            rug=True)
+        myax.set_xlabel('0-10cm Soil Water of Capacity [%]')
+        myax.axvline(wb['sw1_percent'].mean(), color='r')
+        myax.set_xlim(0, 100)
+
+        # ---------------------------------------------------------
+        myax = ax[1, 0]
+        sns.distplot(
+            wb['sw2'], hist=False, color="g", ax=myax,
+            rug=True)
+        myax.set_xlabel('10-20cm Soil Water [mm]')
+        myax.axvline(wb['sw2'].mean(), color='r')
+        myax.set_xlim(0, 60)
+
+        myax = ax[1, 1]
+        sns.distplot(
+            wb['sw2_percent'], hist=False, color="g", ax=myax,
+            rug=True)
+        myax.set_xlabel('10-20cm Soil Water of Capacity [%]')
+        myax.axvline(wb['sw2_percent'].mean(), color='r')
+        myax.set_xlim(0, 100)
+
+        # -------------------------------------------------------
+        myax = ax[2, 0]
+        sns.distplot(
+            wb['sw'], hist=False, color="g", ax=myax,
+            rug=True)
+        myax.set_xlabel('Total Soil Water [mm]')
+        myax.axvline(wb['sw'].mean(), color='r')
+        myax.set_xlim(150, 650)
+
+        myax = ax[2, 1]
+        sns.distplot(
+            wb['sw_percent'], hist=False, color="g", ax=myax,
+            rug=True)
+        myax.set_xlabel('Total Soil Water of Capacity [%]')
+        myax.axvline(wb['sw_percent'].mean(), color='r')
+        myax.set_xlim(0, 100)
+
+        plt.setp(ax, yticks=[])
+        plt.tight_layout()
+        fig.savefig('frames/%05i.png' % (doy - 1, ))
+        plt.close()
 
 
 if __name__ == '__main__':
