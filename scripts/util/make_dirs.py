50291e94970129832cac1b82e497cdf0ed68e8e9
akrherz <akrherz@iastate.edu>
2019-08-29 13:07:41 -0500

    sundry cleanup

--- scripts/util/make_dirs.py
+++ scripts/util/make_dirs.py
@@ -2,18 +2,21 @@
 import os
 import sys
 
-from pyiem.util import get_dbconn
+from pyiem.util import get_dbconn, logger
 from pyiem.dep import load_scenarios
-
+LOG = logger()
 PREFIXES = 'crop env man prj run slp sol wb error ofe yld'.split()
 
 
 def do_huc12(scenario, huc12):
     """ Directory creator! """
+    created = 0
     for prefix in PREFIXES:
         newdir = "/i/%s/%s/%s/%s" % (scenario, prefix, huc12[:8], huc12[8:])
         if not os.path.isdir(newdir):
+            created += 1
             os.makedirs(newdir)
+    return created
 
 
 def main(argv):
@@ -27,8 +30,13 @@ def main(argv):
         SELECT distinct huc_12 from flowpaths
         WHERE scenario = %s
     """, (int(sdf.at[scenario, 'huc12_scenario']),))
+    added = 0
     for row in cursor:
-        do_huc12(scenario, row[0])
+        added += do_huc12(scenario, row[0])
+    LOG.info(
+        "scenario: %s Added %s directories over %s HUC12s",
+        scenario, added, cursor.rowcount
+    )
 
 
 if __name__ == '__main__':
