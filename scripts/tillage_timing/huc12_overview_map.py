1425252271449e35a7b5cf6199cc155f2d4760aa
akrherz <akrherz@iastate.edu>
2019-10-09 10:36:27 -0500

    more tillage scenario work #67

---
+++ scripts/tillage_timing/huc12_overview_map.py
@@ -0,0 +1,61 @@
+"""General HUC12 mapper"""
+from __future__ import print_function
+
+import datetime
+import sys
+
+from pyiem.plot.use_agg import plt
+from pyiem.plot import MapPlot, nwsprecip
+from pyiem.util import get_dbconn
+from geopandas import read_postgis
+from shapely.wkb import loads
+import numpy as np
+import cartopy.crs as ccrs
+from matplotlib.patches import Polygon
+import matplotlib.colors as mpcolors
+
+MYHUCS = [x.strip() for x in open('myhucs.txt').readlines()]
+
+
+def main(argv):
+    """Do Great Things"""
+    pgconn = get_dbconn('idep')
+    cursor = pgconn.cursor()
+
+    mp = MapPlot(
+        continentalcolor='#EEEEEE', nologo=True,
+        subtitle='3 HUC12s choosen per MLRA', nocaption=True,
+        title=('Tillage Timing Experiment 30 Random HUC12s'))
+
+    df = read_postgis("""
+        SELECT geom, mlra_id from mlra WHERE mlra_id in (
+            select distinct mlra_id from huc12
+            where huc_12 in %s and scenario = 0)
+    """, pgconn, params=(tuple(MYHUCS), ), geom_col='geom',
+                      index_col='mlra_id')
+    for _i, row in df.iterrows():
+        for poly in row['geom']:
+            arr = np.asarray(poly.exterior)
+            points = mp.ax.projection.transform_points(
+                ccrs.Geodetic(), arr[:, 0], arr[:, 1])
+            p = Polygon(points[:, :2], fc='None', ec='k', zorder=2, lw=0.2)
+            mp.ax.add_patch(p)
+
+    df = read_postgis("""
+        select huc_12, ST_transform(geom, 4326) as geom from huc12
+        where huc_12 in %s and scenario = 0
+    """, pgconn, params=(tuple(MYHUCS), ), geom_col='geom',
+                      index_col='huc_12')
+    for _i, row in df.iterrows():
+        for poly in row['geom']:
+            arr = np.asarray(poly.exterior)
+            points = mp.ax.projection.transform_points(
+                ccrs.Geodetic(), arr[:, 0], arr[:, 1])
+            p = Polygon(points[:, :2], fc='r', ec='k', zorder=3, lw=0.5)
+            mp.ax.add_patch(p)
+
+    mp.postprocess(filename='test.png')
+
+
+if __name__ == '__main__':
+    main(sys.argv)
---
+++ scripts/tillage_timing/huc12results.py
@@ -0,0 +1,33 @@
+"""Create summary tables."""
+
+from pyiem.util import get_dbconn
+from pandas.io.sql import read_sql
+
+
+def main():
+    """Go Main Go."""
+    years = 1.0
+    pgconn = get_dbconn('idep')
+
+    scens = list(range(81, 91))
+    scens.insert(0, 0)
+    myhucs = [x.strip() for x in open('myhucs.txt').readlines()]
+    for scenario in scens:
+        df = read_sql("""
+            SELECT
+            sum(avg_loss) * 4.463 as detach,
+            sum(avg_delivery) * 4.463 as delivery,
+            sum(avg_runoff) as runoff
+            from results_by_huc12 r
+            WHERE r.valid >= '2018-01-01'
+            and r.valid < '2019-01-01' and scenario = %s and huc_12 in %s
+        """, pgconn, params=(scenario, tuple(myhucs)))
+        row = df.iloc[0]
+        div = years * 30.
+        print("%s %6.2f %6.2f %6.2f" % (
+            scenario, row['runoff'] / div, row['delivery'] / div,
+            row['detach'] / div))
+
+
+if __name__ == '__main__':
+    main()
