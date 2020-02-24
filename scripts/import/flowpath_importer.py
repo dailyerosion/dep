eb20b2563eb7a0f9f2b147275e70e47cf70ce72f
akrherz <akrherz@iastate.edu>
2020-02-24 14:46:44 -0600

    non-func refactor

--- scripts/import/flowpath_importer.py
+++ scripts/import/flowpath_importer.py
@@ -75,6 +75,23 @@ def get_data(filename):
       gpd.DataFrame with the geojson data included.
     """
     df = gpd.read_file(filename, index="OBJECTID")
+    # Compute full rotation string
+    # OK, be careful here. Presently, the 8 char field covers
+    # 2010 thru 2017, so we rotate to cover the first and last years
+    # 2007 2011[1]
+    # 2008 2010[0]
+    # 2009 2011[1]
+    # 2018 2016[6]
+    # 2019 2017[7]
+    # 2020 2018[6]
+    s = df["CropRotatn_CY_2017"]
+    df["landuse"] = (
+        s.str[1] + s.str[0] + s.str[1] + s + s.str[6] + s.str[7] + s.str[6]
+    )
+    s = df["Management_CY_2017"]
+    df["management"] = (
+        s.str[1] + s.str[0] + s.str[1] + s + s.str[6] + s.str[7] + s.str[6]
+    )
     return df
 
 
@@ -98,6 +115,83 @@ def delete_previous(cursor, huc12):
     )
 
 
+def process_flowpath(cursor, huc12, db_fid, df):
+    """Do one flowpath please."""
+    lencolname = "%sLen%s" % (PREFIX, huc12)
+    elevcolname = "ep3m%s" % (huc12,)
+    gordcolname = "gord_%s" % (huc12,)
+    # Sort along the length column, which orders the points from top
+    # to bottom
+    df = df.sort_values(lencolname, ascending=True)
+    # Remove any previous data for this flowpath
+    cursor.execute(
+        "DELETE from flowpath_points WHERE flowpath = %s", (db_fid,)
+    )
+    linestring = []
+    sz = len(df.index)
+    maxslope = 0
+    elev_change = 0
+    x_change = 0
+    for segid, (_, row) in enumerate(df.iterrows()):
+        if (segid + 1) == sz:  # Last row!
+            # This effectively repeats the slope of the previous point
+            row2 = df.iloc[segid - 1]
+        else:
+            row2 = df.iloc[segid + 1]
+        dy = abs(row[elevcolname] - row2[elevcolname])
+        elev_change += dy
+        dx = abs(row2[lencolname] - row[lencolname])
+        x_change += dx
+        gridorder = row[gordcolname]
+        if gridorder > TRUNC_GRIDORDER_AT or pd.isnull(gridorder):
+            continue
+        slope = dy / dx
+
+        if slope > maxslope:
+            maxslope = slope
+        args = (
+            db_fid,
+            segid,
+            row[elevcolname] / 100.0,
+            row[lencolname] / 100.0,
+            row["SOL_FY_2018"],
+            row["management"],
+            slope,
+            row["geometry"].x,
+            row["geometry"].y,
+            row["landuse"],
+            SCENARIO,
+            gridorder,
+        )
+        cursor.execute(INSERT_SQL, args)
+
+        linestring.append("%s %s" % (row["geometry"].x, row["geometry"].y))
+
+    # Line string must have at least 2 points
+    if len(linestring) > 1:
+        if x_change == 0:
+            print()
+            print(df)
+            sys.exit()
+        sql = """
+            UPDATE flowpaths SET geom = 'SRID=5070;LINESTRING(%s)',
+            max_slope = %s, bulk_slope = %s
+            WHERE fid = %s
+        """ % (
+            ",".join(linestring),
+            maxslope,
+            elev_change / x_change,
+            db_fid,
+        )
+        cursor.execute(sql)
+    else:
+        # Cull our work above if this flowpath is too short
+        cursor.execute(
+            "DELETE from flowpath_points where flowpath = %s", (db_fid,)
+        )
+        cursor.execute("DELETE from flowpaths where fid = %s", (db_fid,))
+
+
 def process(cursor, filename, huc12df):
     """Processing of a HUC12's data into the database
 
@@ -110,121 +204,17 @@ def process(cursor, filename, huc12df):
       None
     """
     # We get the huc12 code based on the filename
-    huc12 = filename.split(".")[0].split("_")[1]
+    huc12 = filename.split(".")[0].split("_")[-1]
     delete_previous(cursor, huc12)
     # the inbound dataframe has lots of data, one row per flowpath point
     # We group the dataframe by the column which uses a PREFIX and the huc8
-    for flowpath, df in huc12df.groupby("%s%s" % (PREFIX, huc12)):
-        # never do flowpath zero!
-        if flowpath == 0:
+    for flowpath_num, df in huc12df.groupby("%s%s" % (PREFIX, huc12)):
+        # These are upstream errors I should ignore
+        if flowpath_num == 0 or len(df.index) < 2:
             continue
-        # Sort along the length column, which orders the points from top
-        # to bottom
-        df = df.sort_values("%sLen%s" % (PREFIX, huc12), ascending=True)
         # Get or create the flowpathid from the database
-        fid = get_flowpath(cursor, huc12, flowpath)
-        # Remove any previous data for this flowpath
-        cursor.execute(
-            """
-            DELETE from flowpath_points WHERE flowpath = %s
-            and scenario = %s
-        """,
-            (fid, SCENARIO),
-        )
-        linestring = []
-        sz = len(df.index)
-        for segid, (_, row) in enumerate(df.iterrows()):
-            if (segid + 1) == sz:  # Last row!
-                # This effectively repeats the slope of the previous point
-                # by having a negative dx and negative dy below. <hack>
-                row2 = df.iloc[segid - 1]
-            else:
-                row2 = df.iloc[segid + 1]
-            dy = row["ep3m%s" % (huc12,)] - row2["ep3m%s" % (huc12,)]
-            dx = (
-                row2["%sLen%s" % (PREFIX, huc12)]
-                - row["%sLen%s" % (PREFIX, huc12)]
-            )
-            # gridorder = 4
-            # Some optional code here that was used for the grid order work
-            gridorder = row["gord_%s" % (huc12,)]
-            if gridorder > TRUNC_GRIDORDER_AT or pd.isnull(gridorder):
-                continue
-            if dx == 0:
-                slope = 0
-            else:
-                slope = dy / dx
-
-            lu = row["CropRotatn_CY_2017"].strip()
-            # Don't allow points without a rotation
-            if lu == "":
-                continue
-
-            # OK, be careful here. Presently, the 8 char field covers
-            # 2010 thru 2017, so we rotate to cover the first and last years
-            # 2007 2011[1]
-            # 2008 2010[0]
-            # 2009 2011[1]
-            # 2018 2016[6]
-            # 2019 2017[7]
-            # 2020 2018[6]
-            full_lu = "%s%s%s%s%s%s%s" % (
-                lu[1],
-                lu[0],
-                lu[1],
-                lu,
-                lu[6],
-                lu[7],
-                lu[6],
-            )
-            # Careful here too, need to spread out the management as above
-            m = row["Management_CY_2017"].strip()
-            full_m = "%s%s%s%s%s%s%s" % (m[1], m[0], m[1], m, m[6], m[7], m[6])
-            args = (
-                fid,
-                segid,
-                row["ep3m%s" % (huc12,)] / 100.0,
-                row["%sLen%s" % (PREFIX, huc12)] / 100.0,
-                row["SOL_FY_2018"],
-                full_m,
-                slope,
-                row["geometry"].x,
-                row["geometry"].y,
-                full_lu,
-                SCENARIO,
-                gridorder,
-            )
-            cursor.execute(INSERT_SQL, args)
-
-            linestring.append("%s %s" % (row["geometry"].x, row["geometry"].y))
-
-        # Line string must have at least 2 points
-        if len(linestring) > 1:
-            sql = """
-                UPDATE flowpaths SET geom = 'SRID=5070;LINESTRING(%s)'
-                WHERE fid = %s and scenario = %s
-            """ % (
-                ",".join(linestring),
-                fid,
-                SCENARIO,
-            )
-            cursor.execute(sql)
-        else:
-            # Cull our work above if this flowpath is too short
-            cursor.execute(
-                """
-                DELETE from flowpath_points
-                where flowpath = %s and scenario = %s
-            """,
-                (fid, SCENARIO),
-            )
-            cursor.execute(
-                """
-                DELETE from flowpaths where fid = %s
-                and scenario = %s
-            """,
-                (fid, SCENARIO),
-            )
+        db_fid = get_flowpath(cursor, huc12, flowpath_num)
+        process_flowpath(cursor, huc12, db_fid, df)
     return huc12
 
 
@@ -232,7 +222,7 @@ def main():
     """Our main function, the starting point for code execution"""
     cursor = PGCONN.cursor()
     # track our work
-    with open("myhucs.txt", "w") as fp:
+    with open("myhucs.txt", "w") as fh:
         # Change the working directory to where we have data files
         os.chdir("../../data/%s" % (sys.argv[2],))
         # collect up the GeoJSONs in that directory
@@ -251,7 +241,7 @@ def main():
                 cursor = PGCONN.cursor()
             df = get_data(fn)
             huc12 = process(cursor, fn, df)
-            fp.write("%s\n" % (huc12,))
+            fh.write("%s\n" % (huc12,))
             i += 1
 
     # Commit the database changes
