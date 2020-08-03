708a9e6c07c376e66fde6f7b02ff04482ddc888d
akrherz <akrherz@iastate.edu>
2020-08-03 13:19:34 -0500

    add util dumper

---
+++ scripts/util/dump_wb.py
@@ -0,0 +1,35 @@
+"""Dump Water Balance for Dr Wang
+
+070802070603 2016-2018 March-June
+"""
+import os
+import glob
+
+from pyiem.dep import read_wb
+import pandas as pd
+
+
+def main():
+    """Go Main Go."""
+    os.chdir("/i/0/wb/07080207/0603")
+    frames = []
+    for fn in glob.glob("*.wb"):
+        df = read_wb(fn)
+        # Only worry about OFE1 for now
+        df = df[df["ofe"] == 1]
+        frames.append(df)
+    df = pd.concat(frames)
+    rows = []
+    for year in range(2016, 2019):
+        for day in pd.date_range(
+            f"{year}/03/01", f"{year}/07/01", closed="left"
+        ):
+            row = df[df["date"] == day].mean()
+            row["date"] = day
+            rows.append(row)
+    df = pd.DataFrame(rows)
+    df.to_csv("/tmp/test.csv")
+
+
+if __name__ == "__main__":
+    main()
