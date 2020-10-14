eb195bd033ab98139e4721d14ae8ea5aeb0ffb37
akrherz <akrherz@iastate.edu>
2020-10-14 07:57:58 -0500

    add summary scripts

---
+++ scripts/util/huc12_flowpath_stats.py
@@ -0,0 +1,36 @@
+"""Summarize Flowpath stats."""
+
+from pyiem.util import get_dbconn
+import pandas as pd
+
+
+def main():
+    """Go Main Go."""
+    df = pd.read_excel("isbi.xlsx")
+    dbconn = get_dbconn("idep")
+    cursor = dbconn.cursor()
+    for idx, row in df.iterrows():
+        huc12 = f"{row['huc12Code']:012.0f}"
+        cursor.execute(
+            "SELECT count(*), max(bulk_slope), max(max_slope), "
+            "avg(bulk_slope), avg(max_slope) from flowpaths where "
+            "scenario = 0 and huc_12 = %s",
+            (huc12,),
+        )
+        data = cursor.fetchone()
+        for i, col in enumerate(
+            [
+                "flowpath_count",
+                "max_bulk_slope",
+                "max_max_slope",
+                "avg_bulk_slope",
+                "avg_max_slope",
+            ]
+        ):
+            df.at[idx, col] = data[i]
+
+    df.to_csv("isbi.csv", index=False)
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/util/huc12_summary.py
@@ -0,0 +1,73 @@
+"""Generate DEP summary."""
+from calendar import month_abbr
+
+from pyiem.dep import read_env
+from pyiem.util import get_dbconn
+import pandas as pd
+from pandas.io.sql import read_sql
+from tqdm import tqdm
+
+
+def main():
+    """Go Main Go."""
+    dbconn = get_dbconn("idep")
+    gpd = read_sql(
+        "SELECT huc_12 from huc12 where states ~* 'MN' and scenario = 0",
+        dbconn,
+        index_col=None,
+    )
+    result = []
+    for _, row in tqdm(gpd.iterrows(), total=len(gpd.index)):
+        # Find flowpaths intersecting the shape bounds
+        df = read_sql(
+            "SELECT fpath, ST_Length(geom) as len from flowpaths "
+            "where scenario = 0 and huc_12 = %s",
+            dbconn,
+            params=(row["huc_12"],),
+            index_col=None,
+        )
+        if df.empty:
+            continue
+        res = {"fpcount": len(df.index), "HUC_12": row["huc_12"]}
+        # Read all the envs, include row count
+        envs = []
+        for _, fp in df.iterrows():
+            envfn = (
+                f"/i/0/env/{row['huc_12'][:8]}/{row['huc_12'][8:]}/"
+                f"{row['huc_12']}_{fp['fpath']:.0f}.env"
+            )
+            envdf = read_env(envfn)
+            envdf["key"] = f"{row['huc_12']}_{fp['fpath']}"
+            envdf["delivery"] = envdf["sed_del"] / fp["len"]
+            envs.append(envdf)
+        envdf = pd.concat(envs)
+        envdf["month"] = envdf["date"].dt.month
+        # HUC 12 Annual Soils Loss, Precipitation and RunOff for 2008-2019
+        genvdf = envdf.groupby(by=["key", "year"]).sum().groupby("year").mean()
+        for year in range(2008, 2020):
+            ydf = genvdf.loc[year]
+            for col in ["delivery", "precip", "runoff"]:
+                res[f"{col}_{year}"] = float(ydf[col])
+        # HUC 12 and headwater lake catchments monthly Soils Loss,
+        # Precipitation and RunOff for 2019
+        env2019 = envdf[envdf["year"] == 2019]
+        genv2019 = (
+            env2019.groupby(by=["key", "month"]).sum().groupby("month").mean()
+        )
+        for month in range(1, 13):
+            if month not in genv2019.index.values:
+                continue
+            mdf = genv2019.loc[month]
+            label = month_abbr[month].lower()
+            for col in ["delivery", "precip", "runoff"]:
+                res[f"{col}_{year}_{label}"] = float(mdf[col])
+        result.append(res)
+
+    # Dump out
+    df = pd.DataFrame(result)
+    df = df.fillna(0)
+    df.to_csv("dep_huc12_data.csv", index=False, float_format="%.4f")
+
+
+if __name__ == "__main__":
+    main()
