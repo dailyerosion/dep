1cb558a28100918ab2dc146db570b07b9dbd4af9
akrherz <akrherz@iastate.edu>
2020-10-17 08:08:15 -0500

    add STD columns per request

--- scripts/util/huc12_flowpath_stats.py
+++ scripts/util/huc12_flowpath_stats.py
@@ -6,14 +6,15 @@ import pandas as pd
 
 def main():
     """Go Main Go."""
-    df = pd.read_excel("isbi.xlsx")
+    df = pd.read_csv("isbi.csv")
     dbconn = get_dbconn("idep")
     cursor = dbconn.cursor()
     for idx, row in df.iterrows():
         huc12 = f"{row['huc12Code']:012.0f}"
         cursor.execute(
             "SELECT count(*), max(bulk_slope), max(max_slope), "
-            "avg(bulk_slope), avg(max_slope) from flowpaths where "
+            "avg(bulk_slope), avg(max_slope), stddev(bulk_slope), "
+            "stddev(max_slope) from flowpaths where "
             "scenario = 0 and huc_12 = %s",
             (huc12,),
         )
@@ -25,6 +26,8 @@ def main():
                 "max_max_slope",
                 "avg_bulk_slope",
                 "avg_max_slope",
+                "std_bulk_slope",
+                "std_max_slope",
             ]
         ):
             df.at[idx, col] = data[i]
