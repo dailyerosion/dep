24d108e28035112bfd8463927dd5a3a258295559
akrherz <akrherz@iastate.edu>
2022-02-19 06:03:21 -0600

    add offline wind dumper

---
+++ scripts/sweep/dump_wind.py
@@ -0,0 +1,28 @@
+"""Dump out hourly wind for a year for Dr Wilson."""
+
+import requests
+import pandas as pd
+from tqdm import tqdm
+
+IEMRE = "http://mesonet.agron.iastate.edu/iemre/hourly"
+
+
+def main():
+    """Go Main Go."""
+    lat = 48.75
+    lon = -97.5
+    progress = tqdm(pd.date_range("2021-01-01", "2021-12-31"))
+    for dt in progress:
+        progress.set_description(dt.strftime("%Y-%m-%d"))
+        uri = f"{IEMRE}/{dt:%Y-%m-%d}/{lat:.2f}/{lon:.2f}/json"
+        res = requests.get(uri).json()
+        with open(f"wind/{dt:%Y%m%d}.txt", "w", encoding="utf-8") as fh:
+            for i, et in enumerate(res["data"]):
+                vel = (et["uwnd_mps"] ** 2 + et["vwnd_mps"] ** 2) ** 0.5
+                fh.write(f"{vel:.2f} ")
+                if i > 0 and (i + 1) % 6 == 0:
+                    fh.write("\n")
+
+
+if __name__ == "__main__":
+    main()
