46c723e4f5464dd82e5d26108e5d5cbf0f2f26c9
akrherz <akrherz@iastate.edu>
2022-03-17 08:51:14 -0500

    add fbnd to output, wip

--- scripts/util/dump_ofe_results.py
+++ scripts/util/dump_ofe_results.py
@@ -5,7 +5,7 @@ import sys
 
 import pandas as pd
 from tqdm import tqdm
-from pyiem.util import get_dbconnstr
+from pyiem.util import get_sqlalchemy_conn
 from pyiem.dep import read_ofe
 
 # 2007 is skipped
@@ -18,7 +18,7 @@ def main():
     for root, _dirs, files in tqdm(os.walk("/i/0/ofe"), disable=True):
         for filename in files:
             ofedf = read_ofe(f"{root}/{filename}")
-            # Drop any 2007 or 2018+ data
+            # Drop any 2007 or 2021+ data
             ofedf = ofedf[
                 (ofedf["date"] < datetime.datetime(2021, 1, 1))
                 & (ofedf["date"] >= datetime.datetime(2008, 1, 1))
@@ -26,43 +26,52 @@ def main():
             # Figure out the crop string
             fpath = filename.split("_")[1][:-4]
             huc12 = filename[:12]
-            meta = pd.read_sql(
-                """
-                with data as (
-                    select ofe,
-                    (max(elevation) - min(elevation)) /
-                     greatest(3, max(length) - min(length)) as bulk_slope,
-                    max(slope) as max_slope,
-                    max(genlu) as genlu,
-                    max(landuse) as landuse, max(management) as management,
-                    max(surgo) as surgo,
-                    greatest(3, max(length) - min(length)) as length
-                    from flowpath_points p JOIN flowpaths f on
-                    (p.flowpath = f.fid) WHERe f.scenario = 0
-                    and f.huc_12 = %s  and fpath = %s GROUP by ofe)
-                select d.*, g.label from data d JOIN general_landuse g on
-                (d.genlu = g.id)
-                """,
-                get_dbconnstr("idep"),
-                params=(huc12, fpath),
-            )
+            with get_sqlalchemy_conn("idep") as conn:
+                meta = pd.read_sql(
+                    """
+                    with data as (
+                        select ofe,
+                        (max(elevation) - min(elevation)) /
+                        greatest(3, max(length) - min(length)) as bulk_slope,
+                        max(slope) as max_slope,
+                        max(genlu) as genlu,
+                        max(landuse) as landuse, max(management) as management,
+                        max(surgo) as surgo,
+                        max(fbndid) as max_fbndid,
+                        min(fbndid) as min_fbndid,
+                        greatest(3, max(length) - min(length)) as length
+                        from flowpath_points p JOIN flowpaths f on
+                        (p.flowpath = f.fid) WHERe f.scenario = 0
+                        and f.huc_12 = %s  and fpath = %s GROUP by ofe)
+                    select d.*, g.label from data d JOIN general_landuse g on
+                    (d.genlu = g.id)
+                    """,
+                    conn,
+                    params=(huc12, fpath),
+                )
             # could have zeros from bulk_slope, so overwrite max value
             meta.loc[meta["bulk_slope"] == 0, "bulk_slope"] = meta["max_slope"]
 
             for ofe in ofedf["ofe"].unique():
                 myofe = ofedf[ofedf["ofe"] == ofe]
                 meta_ofe = meta[meta["ofe"] == ofe]
+                print(fpath)
                 print(meta_ofe)
                 if meta_ofe.empty:
                     print(ofe, huc12, fpath)
                     sys.exit()
                 length = meta_ofe["length"].values[0]
+                assert (
+                    meta_ofe["min_fbndid"].values[0]
+                    == meta_ofe["max_fbndid"].values[0]
+                )
                 rows.append(
                     {
                         "id": f"{filename[:-4]}_{ofe}",
                         "huc12": huc12,
                         "fpath": fpath,
                         "ofe": ofe,
+                        "fbndid": meta_ofe["min_fbndid"].values[0],
                         "CropRotationString": meta_ofe["landuse"].values[0],
                         "bulk_slope[1]": meta_ofe["bulk_slope"].values[0],
                         "max_slope[1]": meta_ofe["max_slope"].values[0],
