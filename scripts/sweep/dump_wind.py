205be614f8b0e6ba0b46cb3648bca892e7ff8123
akrherz <akrherz@iastate.edu>
2022-03-25 15:01:34 -0500

    add diagnostic

--- scripts/sweep/dump_wind.py
+++ scripts/sweep/dump_wind.py
@@ -12,18 +12,26 @@ def main():
     lat = 48.75
     lon = -97.5
     progress = tqdm(pd.date_range("2021-01-01", "2021-12-31"))
+    days = []
     with open("/tmp/wind.txt", "w", encoding="utf-8") as ffh:
         for dt in progress:
             progress.set_description(dt.strftime("%Y-%m-%d"))
             uri = f"{IEMRE}/{dt:%Y-%m-%d}/{lat:.2f}/{lon:.2f}/json"
             res = requests.get(uri).json()
+            hit = False
             with open(f"wind/{dt:%Y%m%d}.txt", "w", encoding="utf-8") as fh:
                 for i, et in enumerate(res["data"]):
                     vel = (et["uwnd_mps"] ** 2 + et["vwnd_mps"] ** 2) ** 0.5
+                    if vel > 10:
+                        hit = True
                     fh.write(f"{vel:.2f} ")
                     if i > 0 and (i + 1) % 6 == 0:
                         fh.write("\n")
                     ffh.write(f"{et['valid_local']},{vel:.2f}\n")
+            if hit:
+                days.append(f"{dt:%Y-%m-%d}")
+    print(f"{len(days)} days with wind > 10 m/s")
+    print(" ".join(days))
 
 
 if __name__ == "__main__":
