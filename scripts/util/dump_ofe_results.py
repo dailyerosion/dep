48576e7ab206e046d555096b3b8dbe31c0c9cd30
akrherz <akrherz@iastate.edu>
2022-10-04 13:03:53 -0500

    use new gssurgo relation

--- scripts/util/dump_ofe_results.py
+++ scripts/util/dump_ofe_results.py
@@ -36,12 +36,16 @@ def main():
                         max(slope) as max_slope,
                         max(genlu) as genlu,
                         max(landuse) as landuse, max(management) as management,
-                        max(surgo) as surgo,
+                        max(g.mukey) as surgo,
+                        max(g.kwfact) as kwfact,
+                        max(g.hydrogroup) as hydrogroup,
                         max(fbndid) as max_fbndid,
                         min(fbndid) as min_fbndid,
                         greatest(3, max(length) - min(length)) as length
                         from flowpath_points p JOIN flowpaths f on
-                        (p.flowpath = f.fid) WHERe f.scenario = 0
+                        (p.flowpath = f.fid)
+                        JOIN gssurgo g on (p.gssurgo_id = g.id)
+                        WHERe f.scenario = 0
                         and f.huc_12 = %s  and fpath = %s GROUP by ofe)
                     select d.*, g.label from data d JOIN general_landuse g on
                     (d.genlu = g.id)
@@ -55,16 +59,11 @@ def main():
             for ofe in ofedf["ofe"].unique():
                 myofe = ofedf[ofedf["ofe"] == ofe]
                 meta_ofe = meta[meta["ofe"] == ofe]
-                print(fpath)
-                print(meta_ofe)
                 if meta_ofe.empty:
                     print(ofe, huc12, fpath)
                     sys.exit()
                 length = meta_ofe["length"].values[0]
-                assert (
-                    meta_ofe["min_fbndid"].values[0]
-                    == meta_ofe["max_fbndid"].values[0]
-                )
+                # TODO does not handle OFEs crossing fbndid values
                 rows.append(
                     {
                         "id": f"{filename[:-4]}_{ofe}",
@@ -76,6 +75,8 @@ def main():
                         "bulk_slope[1]": meta_ofe["bulk_slope"].values[0],
                         "max_slope[1]": meta_ofe["max_slope"].values[0],
                         "soil_mukey": meta_ofe["surgo"].values[0],
+                        "soil_kwfact": meta_ofe["kwfact"].values[0],
+                        "soil_hydrogroup": meta_ofe["hydrogroup"].values[0],
                         "rainfall": -1,
                         "runoff[mm/yr]": myofe["runoff"].sum() / YEARS,
                         "detach": -1,
