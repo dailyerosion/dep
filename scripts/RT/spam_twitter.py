6b61618b6ad1568e75e4b62372ccb96bcc7ebad2
akrherz <akrherz@iastate.edu>
2023-07-20 08:25:55 -0500

    update tweeting to v2

--- scripts/RT/spam_twitter.py
+++ scripts/RT/spam_twitter.py
@@ -3,9 +3,33 @@ import datetime
 from io import BytesIO
 
 import requests
+import tweepy
 from pyiem import util
 
 LOG = util.logger()
+
+
+def get_client():
+    """Do the tweeting."""
+    props = util.get_properties()
+    cursor = util.get_dbconn("mesosite").cursor()
+    cursor.execute(
+        "select access_token, access_token_secret from "
+        "iembot_twitter_oauth WHERE screen_name = 'dailyerosion'",
+    )
+    row = cursor.fetchone()
+    auth = tweepy.OAuth1UserHandler(
+        props.get("bot.twitter.consumerkey"),
+        props.get("bot.twitter.consumersecret"),
+        row[0],
+        row[1],
+    )
+    return auth, tweepy.Client(
+        consumer_key=props.get("bot.twitter.consumerkey"),
+        consumer_secret=props.get("bot.twitter.consumersecret"),
+        access_token=row[0],
+        access_token_secret=row[1],
+    )
 
 
 def main():
@@ -13,7 +37,8 @@ def main():
     if datetime.date.today().month < 4 or datetime.date.today().month > 10:
         LOG.warning("Tweeting disabled Nov-Mar")
         return
-    twitter = util.get_twitter("dailyerosion")
+    auth, twitter = get_client()
+    v1client = tweepy.API(auth)
     # assume we run for yesterday
     yesterday = datetime.date.today() - datetime.timedelta(days=1)
     yyyymmdd = yesterday.strftime("%Y%m%d")
@@ -29,17 +54,17 @@ def main():
         bio = BytesIO()
         bio.write(req.content)
         bio.seek(0)
-        response = twitter.upload_media(media=bio)
-        media_ids.append(response["media_id"])
+        media = v1client.media_upload(f"{vname}.png", file=bio)
+        media_ids.append(media.media_id)
 
     status = (
         f"Daily Erosion output for {yesterday:%B %-d %Y} is available "
         f"https://dailyerosion.org/map/#{yyyymmdd}//qc_precip"
     )
 
-    res = twitter.update_status(status=status, media_ids=media_ids)
+    res = twitter.create_tweet(text=status, media_ids=media_ids)
     LOG.warning(
-        "Posted https://twitter.com/dailyerosion/status/%s", res["id_str"]
+        "Posted https://twitter.com/dailyerosion/status/%s", res.data["id"]
     )
 
 
