89e83f23fcf511e2f230011f86cce00b6b627787
akrherz <akrherz@iastate.edu>
2024-01-19 15:14:50 -0600

    add one-off util

---
+++ scripts/util/rsync_ofe_env_here.py
@@ -0,0 +1,35 @@
+"""Bring env/ofe files to my laptop."""
+import os
+import subprocess
+
+from tqdm import tqdm
+
+
+def main():
+    """Go Main Go."""
+    myhucs = []
+    with open("myhucs.txt") as hucs:
+        for line in hucs:
+            myhucs.append(line.strip())
+
+    progress = tqdm(myhucs)
+    for huc in progress:
+        progress.set_description(huc)
+        huc8 = huc[:8]
+        huc4 = huc[8:]
+        for mydir in ["ofe", "env"]:
+            localdir = f"/i/0/{mydir}/{huc8}/{huc4}"
+            if not os.path.isdir(localdir):
+                os.makedirs(localdir)
+            cmd = [
+                "rsync",
+                "-a",
+                "--delete",
+                f"mesonet@metvm2-dc:/i/0/{mydir}/{huc8}/{huc4}/.",
+                f"/i/0/{mydir}/{huc8}/{huc4}/",
+            ]
+            subprocess.call(cmd)
+
+
+if __name__ == "__main__":
+    main()
