f885f96af6bbc0a9bb032a9856479864976f3937
akrherz <akrherz@iastate.edu>
2024-06-10 08:28:27 -0500

    mnt: modernize

--- scripts/cligen/map_clifile_points.py
+++ scripts/cligen/map_clifile_points.py
@@ -2,44 +2,48 @@
 
 import glob
 import os
+from typing import List, Tuple
 
-import cartopy.crs as ccrs
 from pyiem.plot import MapPlot
+from pyiem.reference import Z_OVERLAY
+from tqdm import tqdm
 
 
-def update_grid(ax):
+def get_points() -> Tuple[List[float], List[float]]:
     """Plot vals."""
     os.chdir("/i/0/cli")
     lons = []
     lats = []
-    for mydir in glob.glob("*"):
+    for mydir in tqdm(glob.glob("*")):
         os.chdir(mydir)
         for fn in glob.glob("*.cli"):
             tokens = fn[:-4].split("x")
             lons.append(0 - float(tokens[0]))
             lats.append(float(tokens[1]))
         os.chdir("..")
-    ax.scatter(
-        lons,
-        lats,
-        transform=ccrs.PlateCarree(),
-        zorder=1000,
-        marker="+",
-        color="r",
-    )
-
-
-def draw_map():
-    """make maps, not war."""
-    m = MapPlot(sector="conus", title="4 March 2019 :: DEP Precip Points")
-    update_grid(m.ax)
-    m.postprocess(filename="/tmp/map_clipoints.png")
-    m.close()
+    return lons, lats
 
 
 def main():
-    """Go Main Go."""
-    draw_map()
+    """make maps, not war."""
+    lons, lats = get_points()
+    mp = MapPlot(
+        logo="dep",
+        sector="conus",
+        title="7 June 2024 :: DEP Daily Updated Climate File Locations",
+        subtitle=f"Total Climate Files: {len(lons)}",
+        caption="Daily Erosion Project",
+    )
+    # All points will overwhelm matplotlib, so we need to plot them in chunks
+    for i in tqdm(range(0, len(lons), 1000)):
+        mp.panels[0].scatter(
+            lons[i : i + 1000],
+            lats[i : i + 1000],
+            c="r",
+            s=0.1,
+            zorder=Z_OVERLAY,
+        ).set_rasterized(True)
+    mp.fig.savefig("map_clipoints.png")
 
 
 if __name__ == "__main__":
