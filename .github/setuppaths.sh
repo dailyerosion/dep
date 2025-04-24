c58216120e16d3df8a7a580976650159629937d3
akrherz <akrherz@iastate.edu>
2025-04-24 08:33:45 -0500

    💚 Fix CI problems

--- .github/setuppaths.sh
+++ .github/setuppaths.sh
@@ -1,8 +1,10 @@
 # Create necessary paths
 mkdir -p _data/2/0
 mkdir _data/data
+mkdir _data/mesonet
 sudo ln -s `pwd`/_data /mnt/idep2
 sudo ln -s `pwd`/_data/2 /i
+sudo ln -s `pwd`/_data/mesonet /mesonet
 sudo ln -s `pwd` /opt/dep
 
 mkdir -p /i/0/cli/100x042
