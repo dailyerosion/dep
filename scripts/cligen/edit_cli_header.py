1226fb08f8dcdd158a7d81b1bc0ff2de164a8157
akrherz <akrherz@iastate.edu>
2025-07-23 11:47:33 -0500

    ✨ Iterate on non-US CLI domain work
    refs #346

--- scripts/cligen/edit_cli_header.py
+++ scripts/cligen/edit_cli_header.py
@@ -1,13 +1,12 @@
 """Keep the CLI file headers in sync."""
 
-# stdlib
 import os
-import sys
 
-# Third Party
+import click
 import numpy as np
 import pandas as pd
-from pyiem.iemre import EAST, NORTH, SOUTH, WEST, find_ij, get_dailyc_ncname
+from pyiem.grid.nav import get_nav
+from pyiem.iemre import find_ij, get_dailyc_ncname
 from pyiem.util import convert_value, logger, ncopen
 from tqdm import tqdm
 
@@ -15,13 +14,13 @@ from pydep.util import get_cli_fname
 
 LOG = logger()
 # Untracked here, but this version was used to fix the header column labels
-REV = "HeaderRev: v20230605.1"
+REV = "HeaderRev: v20250723.1"
 
 
-def monthly_avgs(lon, lat):
+def monthly_avgs(lon, lat, domain):
     """Do more work."""
-    i, j = find_ij(lon, lat)
-    with ncopen(get_dailyc_ncname()) as nc:
+    i, j = find_ij(lon, lat, domain)
+    with ncopen(get_dailyc_ncname(domain)) as nc:
         highc = convert_value(
             nc.variables["high_tmpk"][:, j, i], "degK", "degC"
         )
@@ -38,7 +37,7 @@ def monthly_avgs(lon, lat):
         },
         index=pd.date_range("2000/01/01", "2000/12/31"),
     )
-    return df.resample("m").agg(
+    return df.resample("ME").agg(
         {"highc": "mean", "lowc": "mean", "p01d": "sum", "swdn": "mean"}
     )
 
@@ -47,27 +46,23 @@ def get_elevation(lon, lat):
     """Use opentopo."""
     # API services have limits, so alas
     # http://research.jisao.washington.edu/data_sets/elevation/
-    with ncopen("/tmp/elev.americas.5-min.nc") as nc:
+    with ncopen("/tmp/elev.0.25-deg.nc") as nc:
         lons = nc.variables["lon"]  # deg E
         lats = nc.variables["lat"]
-        i = np.digitize(lon + 360, lons)
+        if lon < 0:
+            # Convert to 0-360
+            lon += 360
+        i = np.digitize(lon, lons)
         j = np.digitize(lat, lats)
         elev = nc.variables["data"][0, j, i]
     return elev
 
 
-def process(clifn, check_header):
+def process(lon: float, lat: float, clifn: str, domain: str):
     """Edit the given climate file."""
-    lon, lat = [float(x) for x in os.path.basename(clifn)[:-4].split("x")]
-    lon = 0 - lon
 
     with open(clifn, encoding="utf-8") as fh:
         lines = fh.readlines()
-    # Flag to force editing of the file, even if the header is right.  This
-    # is needed for bootstrapping a single file as the copied file will have
-    # this header, but still need edited
-    if check_header and lines[2].find(REV) > -1:
-        return
     # Set Station Name to something descriptive
     lines[2] = f"Station: Daily Erosion Project 0.01deg Grid Cell {REV}\n"
     # Set Lat/Lon from filename
@@ -79,34 +74,46 @@ def process(clifn, check_header):
         f"{tokens[3]} {tokens[4]} {tokens[5]}\n"
     )
     # Compute monthly avg high, low, radiation, rain
-    df = monthly_avgs(lon, lat)
+    df = monthly_avgs(lon, lat, domain)
     lines[6] = " ".join(f"{x:-5.1f}" for x in df["highc"].values) + "\n"
     lines[8] = " ".join(f"{x:-5.1f}" for x in df["lowc"].values) + "\n"
-    lines[10] = " ".join(f"{x:-5.1f}" for x in df["swdn"].values) + "\n"
-    lines[12] = " ".join(f"{x:-5.1f}" for x in df["p01d"].values) + "\n"
+    if lon > 1000:  # These variables are not ready yet
+        lines[10] = " ".join(f"{x:-5.1f}" for x in df["swdn"].values) + "\n"
+        lines[12] = " ".join(f"{x:-5.1f}" for x in df["p01d"].values) + "\n"
     with open(clifn, "w", encoding="utf-8") as fh:
         fh.write("".join(lines))
 
 
-def omnibus():
+def omnibus(domain: str):
     """Do em all."""
-    progress = tqdm(np.arange(WEST, EAST + 0.01, 0.01))
+    nav = get_nav("IEMRE", domain)
+    progress = tqdm(np.arange(nav.left_edge, nav.right_edge + 0.01, 0.01))
     for lon in progress:
         progress.set_description(f"{lon:.2f}")
-        for lat in np.arange(SOUTH, NORTH + 0.01, 0.01):
-            clifn = get_cli_fname(lon, lat)
+        for lat in np.arange(nav.bottom_edge, nav.top_edge + 0.01, 0.01):
+            try:
+                clifn = get_cli_fname(lon, lat)
+            except ValueError:  # outside of bounds
+                continue
             if not os.path.isfile(clifn):
                 continue
-            process(clifn, True)
+            process(lon, lat, clifn, domain)
 
 
-def main(argv):
+@click.command()
+@click.option("--domain", required=True, help="The domain to edit")
+@click.option("--filename", help="The file to edit")
+@click.option("--lon", type=float, help="Longitude of the grid cell")
+@click.option("--lat", type=float, help="Latitude of the grid cell")
+def main(
+    domain: str, filename: str | None, lon: float | None, lat: float | None
+):
     """We ride or die."""
-    if len(argv) == 1:
-        omnibus()
+    if filename is None:
+        omnibus(domain)
         return
-    process(argv[1], False)
+    process(lon, lat, filename, domain)
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main()
