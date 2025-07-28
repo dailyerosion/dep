93fadd2bc7d0faeec3386d8a746b5ed308311d7c
akrherz <akrherz@iastate.edu>
2025-07-28 11:22:01 -0500

    💚 Address setuppaths issue for CI

--- .github/setuppaths.sh
+++ .github/setuppaths.sh
@@ -1,10 +1,12 @@
+set -x
+set -e
 # Create necessary paths
 mkdir -p _data/2/0
 mkdir _data/data
 mkdir _data/mesonet
 mkdir _data/dep
 sudo ln -s `pwd`/_data /mnt/idep2
-sudo ln -s `pwd`/dep /mnt/dep
+sudo ln -s `pwd`/_data/dep /mnt/dep
 sudo ln -s `pwd`/_data/2 /i
 sudo ln -s `pwd`/_data/mesonet /mesonet
 sudo ln -s `pwd` /opt/dep
