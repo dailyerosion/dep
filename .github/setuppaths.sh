ded55d3f648a1c103840675b8adc3e74d7bfba21
akrherz <akrherz@iastate.edu>
2025-07-28 11:05:35 -0500

    💚 Add /mnt/dep path for CI

--- .github/setuppaths.sh
+++ .github/setuppaths.sh
@@ -2,7 +2,9 @@
 mkdir -p _data/2/0
 mkdir _data/data
 mkdir _data/mesonet
+mkdir _data/dep
 sudo ln -s `pwd`/_data /mnt/idep2
+sudo ln -s `pwd`/dep /mnt/dep
 sudo ln -s `pwd`/_data/2 /i
 sudo ln -s `pwd`/_data/mesonet /mesonet
 sudo ln -s `pwd` /opt/dep
